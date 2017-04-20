f8313dc89cf94e1745f38811c0182445f76afe91
LucasBrazi06 <sergefabiano@free.fr>
2017-04-20 17:57:02 +0200

    Make save of meters synchronous

--- server/soap/services/centralSystemService1.6.js
+++ server/soap/services/centralSystemService1.6.js
@@ -189,11 +189,10 @@ module.exports = { /* Services */
         Logging.logReceivedAction(_moduleName, headers.chargeBoxIdentity, "MeterValues", args, headers);
         // Handle
         global.centralSystemSoap.handleMeterValues(args, headers, req).then(function(result) {
+          // Log
+          Logging.logReturnedAction(_moduleName, headers.chargeBoxIdentity, "MeterValues", result);
+          callback(result);
         });
-
-        // return immediately: issue with timestamp on charging station
-        Logging.logReturnedAction(_moduleName, headers.chargeBoxIdentity, "MeterValues", result);
-        callback(result);
 
         /*
           args = {
