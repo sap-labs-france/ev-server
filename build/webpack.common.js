409aa77c11b6c0994248b3f7a9dccddde85ab54a
LucasBrazi06 <sergefabiano@free.fr>
2017-08-03 23:42:17 +0200

    Adjust configuration

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -22,20 +22,7 @@ const config = {
     path: commonPaths.outputPath
   },
   module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          }, {
-            loader: "css-loader" // translates CSS into CommonJS
-          }, {
-            loader: "sass-loader" // compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+    rules: []
   },
   plugins: [
     new webpack.ProgressPlugin(),
