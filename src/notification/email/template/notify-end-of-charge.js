efa9c2ddccd64d7b5cd82b657b930cbde7873ca2
LucasBrazi06 <sergefabiano@free.fr>
2017-08-26 00:12:09 +0200

    Change EndOfNotif template

--- src/notification/email/template/notify-end-of-charge.js
+++ src/notification/email/template/notify-end-of-charge.js
@@ -1,19 +1,16 @@
-module.exports.subject = "EVSE - Your electric vehicule is fully charged";
+module.exports.subject = "EVSE - Your electric vehicule has finished charging";
 module.exports.html = `
   <html>
     <body>
       Hi <%= (user.firstName?user.firstName:user.name) %>,</br>
       </br>
-      Your electric vehicle which is connected to the charging station <%= chargingStationId %> is fully charged!</br>
+      Your electric vehicle, which is connected to the charging station <a href="<%= evseDashboardChargingStationURL %>"><%= chargingStationId %></a>, has finished charging.</br>
       </br>
       <% if (notifStopTransactionAndUnlockConnector) { %>
         The transaction has been automatically stopped and the connector unlocked.</br>
-        </br>
       <% } else { %>
         You can now stop the transaction and unlock the connector.</br>
-        </br>
       <% } %>
-      You can check the status here: <a href="<%= evseDashboardChargingStationURL %>"><%= chargingStationId %> - Connector <%= connectorId %></a></br>
       </br>
       Best Regards,</br>
       EVSE Admin.
@@ -28,16 +25,13 @@ module.exports.fr_FR.html = `
     <body>
       Bonjour <%= (user.firstName?user.firstName:user.name) %>,</br>
       </br>
-      Votre véhicule électrique qui est connecté sur la borne <%= chargingStationId %> a terminé sa charge !</br>
+      Votre véhicule électrique, qui est connecté sur la borne <a href="<%= evseDashboardChargingStationURL %>"><%= chargingStationId %></a>, a terminé sa charge.</br>
       </br>
       <% if (notifStopTransactionAndUnlockConnector) { %>
         La transaction a été automatiquement stoppée et le connecteur dévérouillé.</br>
-        </br>
       <% } else { %>
         Vous pouvez dès maintenant stopper la transaction et devérouiller le connecteur.</br>
-        </br>
       <% } %>
-      Vous pouvez verifier son statut içi : <a href="<%= evseDashboardChargingStationURL %>"><%= chargingStationId %> - Connecteur <%= connectorId %></a></br>
       </br>
       Cordialement,</br>
       EVSE Admin.
