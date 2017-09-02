9541954247350e28bc61d21853ee49648adc7bbc
LucasBrazi06 <sergefabiano@free.fr>
2017-09-02 23:52:31 +0200

    Removed unused task

--- src/migration/MigrationHandler.js
+++ src/migration/MigrationHandler.js
@@ -1,4 +1,3 @@
-const MigrateStartStopTransactionTask = require('./tasks/MigrateStartStopTransactionTask');
 const Logging = require('../utils/Logging');
 
 class MigrationHandler {
@@ -14,39 +13,19 @@ class MigrationHandler {
       global.storage.getMigrations().then((migrations) => {
         let promises = [];
         // ---------------------------------------------------------------------
-        // Stop/Start Transaction
+        // Migration task
         // ---------------------------------------------------------------------
-        let migrateStartStopTransactionTask = new MigrateStartStopTransactionTask();
-        let migrationFiltered = migrations.filter((migration) => {
-          return migration.name === migrateStartStopTransactionTask.getName();
-        });
-        // Run the migration?
-        if (!migrationFiltered || migrationFiltered.length === 0 || migrationFiltered[0].version !== migrateStartStopTransactionTask.getVersion()) {
-          // Log
-          Logging.logInfo({
-            userFullName: "System", source: "BootStrap", module: "start", method: "-", action: "Migrate",
-            message: `Migration task ${migrateStartStopTransactionTask.getName()} version ${migrateStartStopTransactionTask.getVersion()} is running` });
-          // Yes: Migrate
-          promises.push(migrateStartStopTransactionTask.migrate().then(result => {
-            let migration = {};
-            migration.name = migrateStartStopTransactionTask.getName();
-            migration.version = migrateStartStopTransactionTask.getVersion();
-            migration.timestamp = new Date();
-            // Save
-            return global.storage.saveMigration(migration);
-          }));
-        } else {
-          // Log
-          Logging.logInfo({
-            userFullName: "System", source: "BootStrap", module: "start", method: "-", action: "Migrate",
-            message: `Migration task ${migrateStartStopTransactionTask.getName()} version ${migrateStartStopTransactionTask.getVersion()} has already been run` });
-        }
+        // // Log task is running
+        // Logging.logInfo({
+        //   userFullName: "System", source: "BootStrap", module: "start", method: "-", action: "Migrate",
+        //   message: `Migration task ${migrateStartStopTransactionTask.getName()} version ${migrateStartStopTransactionTask.getVersion()} is running` });
+
+        // // Log tasks has already been run
+        // Logging.logInfo({
+        //   userFullName: "System", source: "BootStrap", module: "start", method: "-", action: "Migrate",
+        //   message: `Migration task ${migrateStartStopTransactionTask.getName()} version ${migrateStartStopTransactionTask.getVersion()} has already been run` });
         // Wait
         Promise.all(promises).then((results) => {
-          // Log
-          Logging.logInfo({
-            userFullName: "System", source: "BootStrap", module: "start", method: "-", action: "Migrate",
-            message: `The migration has finished with success` });
           // Ok
           fulfill(results);
         }).catch((error) => {
