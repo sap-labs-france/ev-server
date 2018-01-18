c9d254785311d753ad890dc763e02c755ee01172
LucasBrazi06 <sergefabiano@free.fr>
2018-01-18 23:38:15 +0100

    Transalate End-User Agreement

--- src/end-user-agreement.js
+++ src/end-user-agreement.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"en": `
-		<h1>End-user Agreement for the Usage of the Charge-Angels Monitoring Software</h1>
+		<h1>End-user Agreement for the Usage of the Charge-Angels software</h1>
 		<h2>Information</h2>
 		<p>
 			This chapter cannot be translated into english easily since CNIL is a French principle, the most important part is described in chapter 3.<br/>
@@ -10,16 +10,16 @@ module.exports = {
 			Read the official document : <a href="https://www.cnil.fr/sites/default/files/typo/document/Guide_employeurs_salaries.pdf">Guide</a>
 		</p>
 
-		<h2>How the CNIL applies to our EVSE monitoring application usecase</h2>
+		<h2>How the CNIL applies to the Charge-Angels application usecase</h2>
 
 		<h3>General Principle</h3>
 		<p>
-			Personnal data can be stored only for the purpose of monitoring, optimizing electric vehicle chargers for users.
+			Personnal data will be stored only for the purpose of monitoring and optimizing the charge of the electric vehicles.
 		</p>
 
 		<h3>Keeping meaningfull data</h3>
 		<p>
-			Only meaningfull data will be stored regarding the usage of EVSE charging stations for each user:
+			Only meaningfull data will be stored regarding the usage of the charging stations for each user:
 		</p>
 		<ul>
 			<li>First and Last Name</li>
@@ -27,7 +27,7 @@ module.exports = {
 			<li>Phone and/or the Mobile numbers</li>
 			<li>Badge IDs</li>
 			<li>Photo</li>
-			<li>Plate ID of the Electric Vehicle</li>
+			<li>Plate ID</li>
 			<li>Professional ID Number</li>
 			<li>Cost Center</li>
 		</ul>
@@ -35,48 +35,120 @@ module.exports = {
 			Charging information such as:
 		</p>
 		<ul>
-			<li>Heartbeat, Date, Hour, Duration of the sessions</li>
 			<li>Instant and Total consumption</li>
 			<li>Connector Status</li>
-			<li>Consumption Curves</li>
+			<li>Charging Curves</li>
+			<li>Session Date/Hour and Duration</li>
 			<li>User Sessions</li>
-			<li>Start/Stop actions on Sessions</li>
+			<li>Start/Stop of Sessions</li>
 			<li>Usage and Consumption Statistics</li>
 			<li>Configuration</li>
 			<li>Site Management</li>
-			<li>Price</li>
-			<li>Logging</li>
+			<li>Price Management</li>
 		</ul>
 
 		<h3>Data lifetime</h3>
 		<p>
-			Personal data above will be stored until the end of his/her contract or if the user requests his removal from the system.<br/>
-			Personal data will be removed after that and sessions will be anonymized so no relation can be done with the user anymore.<br/>
+			Personal data will be kept until the end of the contract or on the user's request.<br/>
+			In that case, personal data will be removed and sessions will be anonymized (no relations can be done with the user anymore.)<br/>
 		</p>
 
 		<h3>Security and Non-disclosure principles</h3>
 		<p>
 			The duty of security and the prohibition of processing sensitive data is ensured.<br/>
-			Credentials are required to access the dashboard for either administrators or standard users.<br/>
-			Only administrators will be authorized to read, write, edit, delete all information.<br/>
-			Standard users are only authorized to read their own information such as charging slot availability, charging status of others and charging booking calendar.<br/>
+			Credentials are required to access the application for administrators and standard users.<br/>
+			Only administrators are authorized to read, write, edit, delete all information.<br/>
+			Standard users are only authorized to read and edit their own personnal data, to see the charging stations availability, the current charge, the history of their sessions and their statistics.<br/>
 			Data can be communicated to third party people in case of legal aspects such as "Inspection du travail" or "Services fiscaux" or police services.<br/>
 		</p>
 
 		<h3>Personnal data individual liberties principles</h3>
 
-		<h4>The duty to inform people of their rights</h4>
+		<h4>The duty to inform the users of their rights</h4>
 		<p>
-			This document must be accepted by all users if they want to use available charging stations monitored by this software.<br/>
+			This document must be accepted by all users if they want to use charging stations monitored by this software.<br/>
 		</p>
 
 		<h4>The exercice of the right of access and correction</h4>
 		<p>
 			The holder of the right of access may require that the information concerning him/her that is inaccurate, incomplete, equivocal, no longer valid, or that the use, communication or keeping of which is prohibited, may be corrected, completed, clarified, updated or deleted.<br/>
-			Every person, upon providing proof of his identity, has the right to make inquiry of the services responsible for implementation of automated processing of personnal information, in order to know whether this processing concerns his own personal information and, as applicable, to have it communicated to him/her.<br/>
+			Every person, upon providing proof of his identity, has the right to make inquiry to the services responsible for implementation about which scenarios his/her personal data is used in the application.<br/>
 		</p>
 	`,
+
 	"fr": `
-		<h1>Nouvelle Version Francaise EULA</h1>
-		`
+		<h1>Conditions Générales d'Utilisation de l'application Charge-Angels</h1>
+		<h2>Information</h2>
+		<p>
+				La loi « Informatique et Libertés » du 6 janvier 1978 modifiée par la loi du 6 août 2004 est applicable dès lors qu’il existe un traitement automatisé ou un fichier manuel (c’est-à-dire un fichier informatique ou un fichier « papier ») contenant des informations relatives à des personnes physiques.<br/>
+				Elle définit les principes à respecter lors de la collecte, du traitement et de la conservation de ces données et garantit un certain nombre de droits pour les personnes.<br/>
+				<br/>
+				Lire le document officiel : <a href="https://w ww.cnil.fr/sites/default/files/typo/document/Guide_employeurs_salaries.pdf">Guide</a>;
+		</p>
+
+		<h2>Comment la CNIL s'applique au cas d'utilisation de l'application Charge-Angels</h2>
+
+		<h3>Principe Général</h3>
+		<p>
+				Les données personnelles seront enregistrées seulement pour la gestion de la recharge des véhicules électriques.
+		</p>
+
+		<h3>Conservation des données</h3>
+		<p>
+				Seul les données importantes seront enregistrées concernant l'utilisation des bornes de recharge pour chaque utilisateur.
+		</p>
+		<ul>
+				<li>Nom et Prénom</li>
+				<li>Courrier électronique</li>
+				<li>Téléphone fixe et portable</li>
+				<li>Numéro de badge</li>
+				<li>Photo</li>
+				<li>Plaque d'immatriculation</li>
+				<li>Numéro d'identification professionnel</li>
+				<li>Centre de coût</li>
+		</ul>
+		<p>
+				Informations de recharge comme:
+		</p>
+		<ul>
+				<li>Consommation Instantanée et Totale</li>
+				<li>Statut des Connecteurs</li>
+				<li>Courbes de Charge</li>
+				<li>Date/Heure, Durée des Sessions</li>
+				<li>Historique des Sessions</li>
+				<li>Démarrage/Arrêt des Sessions</li>
+				<li>Statistiques sur l'Utilisation et la Consommation</li>
+				<li>Configuration</li>
+				<li>Gestion des Sites</li>
+				<li>Gestion des Tarifs</li>
+		</ul>
+
+		<h3>Durée de Conservation des Données</h3>
+		<p>
+				Les données personnelles seront conservées jusqu'à la fin du contrat ou à la demande de l'utilisateur.<br/>
+				Dans ce cas, celles-ci seront supprimées et les sessions anonymisées (plus aucunes relations ne pourra être établie avec l'utilisateur).<br/>
+		</p>
+
+		<h3>Sécurité et Principe de non Divulgation</h3>
+		<p>
+				Le devoir de sécurité et l'interdiction sur le traitement des données personnelles est assurés.<br/>
+				Des identifiants sont requis pour accéder à l'application pour les utilisateurs standards et les administrateurs<br/>
+				Seuls les administrateurs sont autorisés à lire, écrire, éditer et effacer toutes les informations<br/>
+				Les utilisateurs standards sont autorisés à lire et modifier leurs propres données personnelles, voir la disponibilité des bornes de recharge, la charge en cours, l'historique de leurs sessions et leurs statistiques.<br/>
+				Les données peuvent être communiquées à une tierce partie en cas de demande légale comme par exemple l'inspection du travail, les services fiscaux ou de police.<br/>
+		</p>
+
+		<h3>Principe de la Liberté des Données Personnelles</h3>
+
+		<h4>Le devoir d'informer les utilisateurs de leurs droits</h4>
+		<p>
+				Ce document doit être accepté par tous les utilisateurs voulant utiliser les bornes de recharges gérées par cette application.<br/>
+		</p>
+
+		<h4>Le droit d'Accès et de Correction</h4>
+		<p>
+				Le détenteur des données peut requérir que les informations le/la concernant qui ne sont pas exactes, incomplètes, équivoques, invalides, ou que l'usage, la communication ou la conservation de ce qui est interdit, peuvent être corrigées, complétées, clarifiées, mises à jour ou effacées.<br/>
+				Chaque personne, pouvant prouver son identité, à le droit de requérir, aux services responsables du développement, les scénarios dans lesquels ses données personnelles sont utilisées.<br/>
+		</p>
+	`
 };
