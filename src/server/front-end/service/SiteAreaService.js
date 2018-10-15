a654e7d0a26f1ff48ef8b2841672ba1d9e353b64
FABIANO <serge.fabiano@sap.com>
2018-10-15 18:13:06 +0200

    Adjustment in import after merde of pull request

--- src/server/front-end/service/SiteAreaService.js
+++ src/server/front-end/service/SiteAreaService.js
@@ -9,7 +9,6 @@ const User = require('../../../model/User');
 const ChargingStation = require('../../../model/ChargingStation');
 const Site = require('../../../model/Site');
 const SiteArea = require('../../../model/SiteArea');
-const Site = require('../../../model/Site');
 
 class SiteAreaService {
 	static async handleCreateSiteArea(action, req, res, next) {
