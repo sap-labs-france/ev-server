9281fa57b9b15fc074b3aa8755fa48ffdfdc5bc5
Mehdi Jouan <mehdi.jouan@sap.com>
2018-11-30 23:52:14 +0100

    add currentStateOfCharge in transaction

--- src/entity/Transaction.js
+++ src/entity/Transaction.js
@@ -77,9 +77,11 @@ class Transaction extends AbstractTenantEntity {
       this._model.totalConsumption = this.getTotalConsumption();
       this._model.currentConsumption = this.getCurrentConsumption();
       if (this.hasStateOfCharges()) {
-        this._model.stateOfCharge = this.getStateOfCharge();
         this._model.currentStateOfCharge = this.getCurrentStateOfCharge();
       }
+    }
+    if (this.hasStateOfCharges()) {
+      this._model.stateOfCharge = this.getStateOfCharge();
     }
     if (this._hasPricing()) {
       this._model.priceUnit = this._model.pricing.priceUnit;
@@ -289,7 +291,7 @@ class Transaction extends AbstractTenantEntity {
 
   getStateOfCharge() {
     if (this.hasStateOfCharges()) {
-      return this.getStateOfCharges()[0].value;
+      return this._model.stateOfCharge;
     }
     return undefined;
   }
