b7189127dd39f194b205501efcbf8227b2482e26
PALAZZOLO Patrice <patrice.palazzolo@free.fr>
2018-12-13 14:06:22 +0100

    ocpi - 1st version of settings rest service

--- src/server/rest/service/SettingService.js
+++ src/server/rest/service/SettingService.js
@@ -5,7 +5,6 @@ const AppAuthError = require('../../../exception/AppAuthError');
 const Authorizations = require('../../../authorization/Authorizations');
 const Constants = require('../../../utils/Constants');
 const Setting = require('../../../entity/Setting');
-const Company = require('../../../entity/Company');
 const User = require('../../../entity/User');
 const SettingSecurity = require('./security/SettingSecurity');
 
@@ -96,38 +95,31 @@ class SettingService {
   static async handleGetSettings(action, req, res, next) {
     try {
       // Check auth
-      if (!Authorizations.canListSites(req.user)) {
+      if (!Authorizations.canListSettings(req.user)) {
         // Not Authorized!
         throw new AppAuthError(
           Constants.ACTION_LIST,
-          Constants.ENTITY_SITES,
+          Constants.ENTITY_SETTINGS,
           null,
           560,
-          'SiteService', 'handleGetSites',
+          'SettingService', 'handleGetSettings',
           req.user);
       }
       // Filter
-      const filteredRequest = SiteSecurity.filterSitesRequest(req.query, req.user);
-      // Get the sites
-      const sites = await Site.getSites(req.user.tenantID,
+      const filteredRequest = SettingSecurity.filterSettingsRequest(req.query, req.user);
+      // Get the all settings identifier
+      const settings = await Setting.getSettings(req.user.tenantID,
         {
-          'search': filteredRequest.Search,
-          'userID': filteredRequest.UserID,
-          'withCompany': filteredRequest.WithCompany,
-          'withSiteAreas': filteredRequest.WithSiteAreas,
-          'withChargeBoxes': filteredRequest.WithChargeBoxes,
-          'withUsers': filteredRequest.WithUsers,
-          'excludeSitesOfUserID': filteredRequest.ExcludeSitesOfUserID,
-          'withAvailableChargers': filteredRequest.WithAvailableChargers
+          'search': filteredRequest.Search
         },
         filteredRequest.Limit, filteredRequest.Skip, filteredRequest.Sort);
       // Set
-      sites.result = sites.result.map((site) => site.getModel());
+      settings.result = settings.result.map((setting) => setting.getModel());
       // Filter
-      sites.result = SiteSecurity.filterSitesResponse(
-        sites.result, req.user);
+      settings.result = SettingSecurity.filterSettingsResponse(
+        settings.result, req.user);
       // Return
-      res.json(sites);
+      res.json(settings);
       next();
     } catch (error) {
       // Log
@@ -138,62 +130,37 @@ class SettingService {
   static async handleCreateSetting(action, req, res, next) {
     try {
       // Check auth
-      if (!Authorizations.canCreateSite(req.user)) {
+      if (!Authorizations.canCreateSetting(req.user)) {
         // Not Authorized!
         throw new AppAuthError(
           Constants.ACTION_CREATE,
-          Constants.ENTITY_SITE,
+          Constants.ENTITY_SETTING,
           null,
           560,
-          'SiteService', 'handleCreateSite',
+          'SettingService', 'handleCreateSetting',
           req.user);
       }
       // Filter
-      const filteredRequest = SiteSecurity.filterSiteCreateRequest(req.body, req.user);
-      // Check Company
-      const company = await Company.getCompany(req.user.tenantID, filteredRequest.companyID);
+      const filteredRequest = SettingSecurity.filterSiteCreateRequest(req.body, req.user);
       // Check Mandatory fields
-      Site.checkIfSiteValid(filteredRequest, req);
-      // Found?
-      if (!company) {
-        // Not Found!
-        throw new AppError(
-          Constants.CENTRAL_SERVER,
-          `The Company ID '${filteredRequest.companyID}' does not exist`, 550,
-          'SiteService', 'handleCreateSite', req.user);
-      }
-      // Create site
-      const site = new Site(req.user.tenantID, filteredRequest);
+      Setting.checkIfSettingValid(filteredRequest, req);
+      
+      // Create setting
+      const setting = new Setting(req.user.tenantID, filteredRequest);
       // Update timestamp
-      site.setCreatedBy(new User(req.user.tenantID, {'id': req.user.id}));
-      site.setCreatedOn(new Date());
-      // Get the users
-      const users = [];
-      if (filteredRequest.userIDs) {
-        for (const userID of filteredRequest.userIDs) {
-          // Get User
-          const user = await User.getUser(req.user.tenantID, userID);
-          // Add
-          users.push(user);
-        }
-      }
-      // Set Users
-      site.setUsers(users);
-      // Save Site
-      const newSite = await site.save();
-      // Save Site's Image
-      newSite.setImage(site.getImage());
-      // Save
-      await newSite.saveImage();
+      setting.setCreatedBy(new User(req.user.tenantID, {'id': req.user.id}));
+      setting.setCreatedOn(new Date());
+      // Save Setting
+      const newSetting = await setting.save();
       // Log
       Logging.logSecurityInfo({
         tenantID: req.user.tenantID,
-        user: req.user, module: 'SiteService', method: 'handleCreateSite',
-        message: `Site '${newSite.getName()}' has been created successfully`,
-        action: action, detailedMessages: newSite
+        user: req.user, module: 'SettingService', method: 'handleCreateSetting',
+        message: `Setting '${newSetting.getIdentifier()}' has been created successfully`,
+        action: action, detailedMessages: newSetting
       });
       // Ok
-      res.json(Object.assign({ id: newSite.getID() }, Constants.REST_RESPONSE_SUCCESS));
+      res.json(Object.assign({ id: newSetting.getID() }, Constants.REST_RESPONSE_SUCCESS));
       next();
     } catch (error) {
       // Log
@@ -204,57 +171,41 @@ class SettingService {
   static async handleUpdateSetting(action, req, res, next) {
     try {
       // Filter
-      const filteredRequest = SiteSecurity.filterSiteUpdateRequest(req.body, req.user);
-      // Get Site
-      const site = await Site.getSite(req.user.tenantID, filteredRequest.id);
-      if (!site) {
+      const filteredRequest = SettingSecurity.filterSiteUpdateRequest(req.body, req.user);
+      // Get Setting
+      const setting = await Setting.getSetting(req.user.tenantID, filteredRequest.id);
+      if (!setting) {
         throw new AppError(
           Constants.CENTRAL_SERVER,
-          `The Site with ID '${filteredRequest.id}' does not exist anymore`, 550,
-          'SiteService', 'handleUpdateSite', req.user);
+          `The Setting with ID '${filteredRequest.id}' does not exist anymore`, 550,
+          'SettingService', 'handleUpdateSetting', req.user);
       }
       // Check Mandatory fields
-      Site.checkIfSiteValid(filteredRequest, req);
+      Setting.checkIfSettingValid(filteredRequest, req);
       // Check auth
-      if (!Authorizations.canUpdateSite(req.user, site.getModel())) {
+      if (!Authorizations.canUpdateSetting(req.user, setting.getModel())) {
         // Not Authorized!
         throw new AppAuthError(
           Constants.ACTION_UPDATE,
-          Constants.ENTITY_SITE,
-          site.getID(),
+          Constants.ENTITY_SETTING,
+          setting.getID(),
           560,
-          'SiteService', 'handleUpdateSite',
+          'SettingService', 'handleUpdateSetting',
           req.user);
       }
       // Update
-      Database.updateSite(filteredRequest, site.getModel());
+      Database.updateSetting(filteredRequest, setting.getModel());
       // Update timestamp
-      site.setLastChangedBy(new User(req.user.tenantID, {'id': req.user.id}));
-      site.setLastChangedOn(new Date());
-      // Update Site's Image
-      await site.saveImage();
-      // Get the users
-      const users = [];
-      if (filteredRequest.userIDs) {
-        for (const userID of filteredRequest.userIDs) {
-          // Get User
-          const user = await User.getUser(req.user.tenantID, userID);
-          if (user) {
-            // Add
-            users.push(user);
-          }
-        }
-      }
-      // Set Users
-      site.setUsers(users);
-      // Update Site
-      const updatedSite = await site.save();
+      setting.setLastChangedBy(new User(req.user.tenantID, {'id': req.user.id}));
+      setting.setLastChangedOn(new Date());
+      // Update Setting
+      const updatedSetting = await setting.save();
       // Log
       Logging.logSecurityInfo({
         tenantID: req.user.tenantID,
-        user: req.user, module: 'SiteService', method: 'handleUpdateSite',
-        message: `Site '${updatedSite.getName()}' has been updated successfully`,
-        action: action, detailedMessages: updatedSite
+        user: req.user, module: 'SettingService', method: 'handleUpdateSetting',
+        message: `Setting '${updatedSetting.getIdentifier()}' has been updated successfully`,
+        action: action, detailedMessages: updatedSetting
       });
       // Ok
       res.json(Constants.REST_RESPONSE_SUCCESS);
