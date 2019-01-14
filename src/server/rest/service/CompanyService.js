0950a1e0cde373859c3adce9f72dcd559ab94253
PALAZZOLO Patrice <patrice.palazzolo@free.fr>
2019-01-14 15:42:24 +0100

    site management - add WithLogo param to getCompanies service

--- src/server/rest/service/CompanyService.js
+++ src/server/rest/service/CompanyService.js
@@ -181,7 +181,7 @@ class CompanyService {
       const filteredRequest = CompanySecurity.filterCompaniesRequest(req.query, req.user);
       // Get the companies
       const companies = await Company.getCompanies(req.user.tenantID,
-        { search: filteredRequest.Search, withSites: filteredRequest.WithSites },
+        { search: filteredRequest.Search, withSites: filteredRequest.WithSites, withLogo: filteredRequest.WithLogo },
         filteredRequest.Limit, filteredRequest.Skip, filteredRequest.Sort);
       // Set
       companies.result = companies.result.map((company) => company.getModel());
