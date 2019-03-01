e1e0ff88f3441cca8fb246a69d4a55b65af0ad3b
Mehdi Jouan <mehdi.jouan@sap.com>
2019-03-01 12:07:09 +0100

    move email html template and all sub templates in assets

--- src/notification/email/EMailNotificationTask.js
+++ src/notification/email/EMailNotificationTask.js
@@ -1,22 +1,10 @@
 const email = require("emailjs");
 const ejs = require('ejs');
+const fs = require('fs');
 const BackendError = require('../../exception/BackendError');
 const Configuration = require('../../utils/Configuration');
 const Logging = require('../../utils/Logging');
 const Utils = require('../../utils/Utils');
-const mainTemplate = require('./template/main-template.js');
-const requestPassword = require('./template/request-password.js');
-const chargingStationRegistered = require('./template/charging-station-registered.js');
-const newPassword = require('./template/new-password.js');
-const newRegisteredUser = require('./template/new-registered-user.js');
-const userAccountStatusChanged = require('./template/user-account-status-changed.js');
-const verificationEmail = require('./template/verification-email.js');
-const endOfCharge = require('./template/end-of-charge.js');
-const optimalChargeReached = require('./template/optimal-charge-reached.js');
-const endOfSession = require('./template/end-of-session.js');
-const chargingStationStatusError = require('./template/charging-station-status-error.js');
-const transactionStarted = require('./template/transaction-started');
-const unknownUserBadged = require('./template/unknown-user-badged');
 const NotificationTask = require('../NotificationTask');
 
 require('source-map-support').install();
@@ -108,112 +96,61 @@ class EMailNotificationTask extends NotificationTask {
       throw new BackendError(null, `No User is provided for '${templateName}'`,
         "EMailNotificationTask", "_prepareAndSendEmail");
     }
-    // Get the template dir
-    switch (templateName) {
-      // Request password
-      case 'request-password':
-        // Copy
-        emailTemplate = JSON.parse(JSON.stringify(requestPassword));
-        break;
-      // New password
-      case 'new-password':
-        emailTemplate = JSON.parse(JSON.stringify(newPassword));
-        break;
-      // Registered user
-      case 'new-registered-user':
-        emailTemplate = JSON.parse(JSON.stringify(newRegisteredUser));
-        break;
-      // Verification email
-      case 'verification-email':
-        emailTemplate = JSON.parse(JSON.stringify(verificationEmail));
-        break;
-      // End of charge
-      case 'end-of-charge':
-        emailTemplate = JSON.parse(JSON.stringify(endOfCharge));
-        break;
-      // Optimal Charge Reached
-      case 'optimal-charge-reached':
-        emailTemplate = JSON.parse(JSON.stringify(optimalChargeReached));
-        break;
-      // End of session
-      case 'end-of-session':
-        emailTemplate = JSON.parse(JSON.stringify(endOfSession));
-        break;
-      // Charging Station Status Error
-      case 'charging-station-status-error':
-        emailTemplate = JSON.parse(JSON.stringify(chargingStationStatusError));
-        break;
-      case 'unknown-user-badged':
-        emailTemplate = JSON.parse(JSON.stringify(unknownUserBadged));
-        break;
-      case 'transaction-started':
-        emailTemplate = JSON.parse(JSON.stringify(transactionStarted));
-        break;
-      case 'user-account-status-changed':
-        emailTemplate = JSON.parse(JSON.stringify(userAccountStatusChanged));
-        break;
-      case 'charging-station-registered':
-        emailTemplate = JSON.parse(JSON.stringify(chargingStationRegistered));
-        break;
-    }
+
+    emailTemplate = JSON.parse(fs.readFileSync(`${appRoot}/assets/server/notification/email/${locale}/${templateName}.json`, 'utf8'));
     // Template found?
     if (!emailTemplate) {
       // Error
       throw new BackendError(null, `No Email template found for '${templateName}'`,
         "EMailNotificationTask", "_prepareAndSendEmail");
     }
-    // Check for localized template?
-    if (emailTemplate[locale]) {
-      // Set the localized template
-      emailTemplate = emailTemplate[locale];
-    }
 
     // Render the localized template ---------------------------------------
     // Render the subject
-    emailTemplate.email.subject = ejs.render(emailTemplate.email.subject, data);
+    emailTemplate.subject = ejs.render(emailTemplate.subject, data);
     // Render Base URL
-    emailTemplate.email.baseURL = ejs.render(emailTemplate.email.baseURL, data);
-    emailTemplate.email.body.template = templateName;
+    emailTemplate.baseURL = ejs.render(emailTemplate.baseURL, data);
+    emailTemplate.body.template = templateName;
     // Render the title
-    emailTemplate.email.body.header.title = ejs.render(emailTemplate.email.body.header.title, data);
+    emailTemplate.body.header.title = ejs.render(emailTemplate.body.header.title, data);
     // Charge Angels Logo
-    emailTemplate.email.body.header.image.left.url = ejs.render(emailTemplate.email.body.header.image.left.url, data);
+    emailTemplate.body.header.image.left.url = ejs.render(emailTemplate.body.header.image.left.url, data);
 
     // Company Logo
-    emailTemplate.email.body.header.image.right.url = ejs.render(emailTemplate.email.body.header.image.right.url, data);
+    emailTemplate.body.header.image.right.url = ejs.render(emailTemplate.body.header.image.right.url, data);
     // Render Lines Before Action
-    emailTemplate.email.body.beforeActionLines =
-      emailTemplate.email.body.beforeActionLines.map((beforeActionLine) => {
+    emailTemplate.body.beforeActionLines =
+      emailTemplate.body.beforeActionLines.map((beforeActionLine) => {
         return ejs.render(beforeActionLine, data);
       });
     // Remove extra empty lines
-    Utils.removeExtraEmptyLines(emailTemplate.email.body.beforeActionLines);
+    Utils.removeExtraEmptyLines(emailTemplate.body.beforeActionLines);
     // Render Stats
-    if (emailTemplate.email.body.stats) {
-      emailTemplate.email.body.stats =
-        emailTemplate.email.body.stats.map((stat) => {
+    if (emailTemplate.body.stats) {
+      emailTemplate.body.stats =
+        emailTemplate.body.stats.map((stat) => {
           stat.label = ejs.render(stat.label, data);
           stat.value = ejs.render(stat.value, data);
           return stat;
         });
     }
     // Render Action
-    if (emailTemplate.email.body.action) {
-      emailTemplate.email.body.action.title =
-        ejs.render(emailTemplate.email.body.action.title, data);
-      emailTemplate.email.body.action.url =
-        ejs.render(emailTemplate.email.body.action.url, data);
+    if (emailTemplate.body.action) {
+      emailTemplate.body.action.title =
+        ejs.render(emailTemplate.body.action.title, data);
+      emailTemplate.body.action.url =
+        ejs.render(emailTemplate.body.action.url, data);
     }
     // Render Lines After Action
-    emailTemplate.email.body.afterActionLines =
-      emailTemplate.email.body.afterActionLines.map((afterActionLine) => {
+    emailTemplate.body.afterActionLines =
+      emailTemplate.body.afterActionLines.map((afterActionLine) => {
         return ejs.render(afterActionLine, data);
       });
     // Remove extra empty lines
-    Utils.removeExtraEmptyLines(emailTemplate.email.body.afterActionLines);
+    Utils.removeExtraEmptyLines(emailTemplate.body.afterActionLines);
     // Render the final HTML -----------------------------------------------
-    const subject = ejs.render(mainTemplate.subject, emailTemplate.email);
-    const html = ejs.render(mainTemplate.html, emailTemplate.email);
+    const subject = ejs.render(fs.readFileSync(`${appRoot}/assets/server/notification/email/subject.mustache`, 'utf8'), emailTemplate);
+    const html = ejs.render(fs.readFileSync(`${appRoot}/assets/server/notification/email/template.html`, 'utf8'), emailTemplate);
     // Add Admins in BCC from Configuration
     let adminEmails = null;
     if (data.adminUsers && data.adminUsers.length > 0) {
@@ -238,7 +175,7 @@ class EMailNotificationTask extends NotificationTask {
       // Return one email
       return (data.user ? data.user.email : null);
     } else if (data.users) {
-      // Return a list of emails 
+      // Return a list of emails
       return data.users.map((user) => user.email).join(',');
     }
   }
