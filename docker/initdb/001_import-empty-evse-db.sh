a3fb6cc97afd2513244691b8f8f0b5d328b37b9a
Jérôme Benoit <jerome.benoit@piment-noir.org>
2019-03-11 22:04:22 +0100

    docker: add some build args and env variables.
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- docker/initdb/001_import-empty-evse-db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export_file="export-empty-evse-db.zip"
-mongodb_home=/home/mongodb
-mongodb_name="evse"
-
-unzip -d ${mongodb_home} ${mongodb_home}/${export_file}
-mongorestore -d ${mongodb_name} --authenticationDatabase admin --username admin --password admin --gzip ${mongodb_home}/${mongodb_name}
-rm -rf ${mongodb_home}/${export_file} ${mongodb_home}/${mongodb_name}
