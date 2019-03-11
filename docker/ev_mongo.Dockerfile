7e29bd19e70433dbc2deeb66be58ae2bc36cd8c9
Jérôme Benoit <jerome.benoit@piment-noir.org>
2019-03-11 20:23:36 +0100

    docker: some cleanups in the docker files and variables handling
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- docker/ev_mongo.Dockerfile
+++ docker/ev_mongo.Dockerfile
@@ -5,7 +5,7 @@ COPY initdb/*.sh ./docker-entrypoint-initdb.d/
 RUN mkdir -p /home/mongodb && chown mongodb:mongodb /home/mongodb
 COPY initdb/export-empty-evse-db.zip ./home/mongodb
 RUN chown mongodb:mongodb /home/mongodb/export-empty-evse-db.zip
-RUN apt-get -y update && \
-    apt-get -y install flip unzip
+RUN apt-get -y update \
+    && apt-get -y install flip unzip
 RUN flip -u ./docker-entrypoint-initdb.d/*.sh
 
--- docker/initdb/001_import-empty-evse-db.sh
+++ docker/initdb/001_import-empty-evse-db.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-unzip -d /home/mongodb /home/mongodb/export-empty-evse-db.zip
-mongorestore -d evse --authenticationDatabase admin --username admin --password admin --gzip /home/mongodb/evse
-rm -rf /home/mongodb/export-empty-evse-db.zip /home/mongodb/evse
+export_file="export-empty-evse-db.zip"
+mongodb_home=/home/mongodb
+mongodb_name="evse"
+
+unzip -d ${mongodb_home} ${mongodb_home}/${export_file}
+mongorestore -d ${mongodb_name} --authenticationDatabase admin --username admin --password admin --gzip ${mongodb_home}/${mongodb_name}
+rm -rf ${mongodb_home}/${export_file} ${mongodb_home}/${mongodb_name}
