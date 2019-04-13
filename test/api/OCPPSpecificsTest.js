b6538cfeaac18d035896f7934c56c894c5be2461
FABIANO <serge.fabiano@sap.com>
2019-04-13 12:44:03 +0200

    Updated Specific Unit Tests

--- test/api/OCPPSpecificsTest.js
+++ test/api/OCPPSpecificsTest.js
@@ -10,45 +10,54 @@ const DataHelper = require('./DataHelper');
 const CentralServerService = require('../api/client/CentralServerService');
 const Factory = require('../factories/Factory');
 
-describe('OCPP Specific tests', function() {
+describe('ChargingStation Keba tests', function() {
   this.timeout(10000);
-  
   before(async () => {
   });
 
   after(async () => {
+    this.dataHelper.close();
+    this.dataHelper.destroyData();
   });
 
-  // it('OCPP 1.6 - KEBA Sample.Clock in MeterValues - Should not be taken into account', async () => {
-  //   // const requestHandler = [];
-  //   this.tenantID = await CentralServerService.authenticatedApi.getTenantID();
-  //   const connectorId = 1;
-  //   const tagId = user.tagIDs[0];
-  //   const meterStart = 0;
-  //   const startDate = moment();
-  //   const currentTime = startDate.clone();
-  //   let cumulated = meterStart;
-  //   // Start Transaction
-  //   const transactionId = await this.dataHelper.startTransaction(this.chargingStation, connectorId, tagId, meterStart, startDate);
-  //   // Send consumptions
-  //   await this.dataHelper.sendConsumptionMeterValue(this.chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
-  //   await this.dataHelper.sendConsumptionMeterValue(this.chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
-  //   await this.dataHelper.sendConsumptionMeterValue(this.chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
-  //   await this.dataHelper.sendClockMeterValue(this.chargingStation, connectorId, transactionId, 0, currentTime.clone());
-  //   await this.dataHelper.sendConsumptionMeterValue(this.chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
-  //   await this.dataHelper.stopTransaction(this.chargingStation, transactionId, tagId, cumulated, currentTime.add(1, 'minute').clone());
-  //   // Read the transaction
-  //   const response = await CentralServerService.transactionApi.readById(transactionId);
-  //   // Check
-  //   expect(response.status).to.equal(200);
-  //   expect(response.data).to.deep.containSubset({
-  //     id: transactionId,
-  //     meterStart: meterStart,
-  //     stop: {
-  //       totalConsumption: cumulated - meterStart,
-  //       totalInactivitySecs: 60
-  //     }
-  //   });
-  // });
+
+  it('Should not take care about keba clock meterValues', async () => {
+    // const requestHandler = [];
+    this.tenantID = await CentralServerService.authenticatedApi.getTenantID();
+
+    this.dataHelper = new DataHelper('1.6', this.tenantID);
+    const user = await this.dataHelper.createUser();
+    const company = await this.dataHelper.createCompany();
+    const site = await this.dataHelper.createSite(company, [user]);
+    const chargingStation = await this.dataHelper.createChargingStation(Factory.chargingStation.build({
+      chargePointVendor: 'Keba AG',
+      id: faker.random.alphaNumeric(12)
+    }), 1);
+    await this.dataHelper.createSiteArea(site, [chargingStation]);
+    const connectorId = 1;
+    const tagId = user.tagIDs[0];
+    const meterStart = 0;
+    const startDate = moment();
+    const currentTime = startDate.clone();
+    let cumulated = meterStart;
+    const transactionId = await this.dataHelper.startTransaction(chargingStation, connectorId, tagId, meterStart, startDate);
+    await this.dataHelper.sendConsumptionMeterValue(chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
+    await this.dataHelper.sendConsumptionMeterValue(chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
+    await this.dataHelper.sendConsumptionMeterValue(chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
+    await this.dataHelper.sendClockMeterValue(chargingStation, connectorId, transactionId, 0, currentTime.clone());
+    await this.dataHelper.sendConsumptionMeterValue(chargingStation, connectorId, transactionId, cumulated+=300, currentTime.add(1, 'minute').clone());
+    await this.dataHelper.stopTransaction(chargingStation, transactionId, tagId, cumulated, currentTime.add(1, 'minute').clone());
+
+    const response = await CentralServerService.transactionApi.readById(transactionId);
+    expect(response.status).to.equal(200);
+    expect(response.data).to.deep.containSubset({
+      id: transactionId,
+      meterStart: meterStart,
+      stop: {
+        totalConsumption: cumulated - meterStart,
+        totalInactivitySecs: 60
+      }
+    });
+  });
 });
 
