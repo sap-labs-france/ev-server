031ed8e499da0d1f29879f343d8a95a5f2e4b830
Alexander Kempf <alex21k@seas.upenn.edu>
2019-05-31 09:38:49 -0400

    AppError ported

---
+++ src/exception/AppError.ts
@@ -0,0 +1,16 @@
+export default class AppError extends Error {
+  constructor(
+    readonly source: string,
+    readonly message: string,
+    readonly errorCode: number = 500,
+    readonly module: string = "N/A",
+    readonly method: string = "N/A",
+    readonly user?: any,
+    readonly actionOnUser?: any,
+    readonly action?: any) {
+    super(message);
+  }
+};
+//TODO: As user, actionOnUser and action are not used in any instantiation of
+// AppError anywhere in the app, I cannot infer their types. Therefore, they will
+// be left any until someone using them will modify the types.
