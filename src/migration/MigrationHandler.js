2b41b42b6c2293b1783490adf5eec99a7cd6359f
Jerome Benoit <jerome.benoit@sap.com>
2019-06-04 17:59:55 +0200

    Don't save migrations to the DB twice.
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- src/migration/MigrationHandler.js
+++ src/migration/MigrationHandler.js
@@ -184,14 +184,6 @@ class MigrationHandler {
     // Log in the console also
     // eslint-disable-next-line no-console
     console.log(`Migration Task '${currentMigrationTask.getName()}' Version '${currentMigrationTask.getVersion()}' has run with success in ${totalTaskTimeSecs} secs ${cluster.isWorker ? 'in worker ' + cluster.worker.id : 'in master'}`);
-
-    // Save to the DB
-    await MigrationStorage.saveMigration({
-      name: currentMigrationTask.getName(),
-      version: currentMigrationTask.getVersion(),
-      timestamp: new Date(),
-      durationSecs: totalTaskTimeSecs
-    });
   }
 }
 
