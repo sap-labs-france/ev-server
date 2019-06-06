9e3a7267ffff7e70c458afc7425016834e5c2d86
DEMESSANT <jean.pierre.demessant@sap.com>
2019-06-06 13:54:43 +0200

    #388-Settings-Passwords-Should-be-encrypted-in-DB
    correction

--- src/utils/Safe.js
+++ src/utils/Safe.js
@@ -8,17 +8,25 @@ const CRYPTO_IV = Configuration.getCryptoConfig().iv;
 
 class Safe {
     static encrypt(data){
-        const cipher = crypto.createCipheriv('aes-256-cbc', CRYPTO_KEY, CRYPTO_IV);
-        var dataHashed = cipher.update(data, 'utf8', 'hex');
-        dataHashed += cipher.final('hex');
-        return dataHashed;
+        try {
+            const cipher = crypto.createCipheriv('aes-256-cbc', CRYPTO_KEY, CRYPTO_IV);
+            var dataHashed = cipher.update(data, 'utf8', 'hex');
+            dataHashed += cipher.final('hex');
+            return dataHashed;
+        } catch(error) {
+            console.log(error);
+        }
     }
     static decrypt(data) {
-        const decipher = crypto.createDecipheriv('aes-256-cbc', CRYPTO_KEY, CRYPTO_IV);
-        var dataUnhashed = decipher.update(data, 'hex','utf-8');
-        dataUnhashed += decipher.final('utf-8');
-        return dataUnhashed;
+        try {
+            const decipher = crypto.createDecipheriv('aes-256-cbc', CRYPTO_KEY, CRYPTO_IV);
+            var dataUnhashed = decipher.update(data, 'hex','utf-8');
+            dataUnhashed += decipher.final('utf-8');
+            return dataUnhashed;
+        } catch(error) {
+            console.log(error);
         }
+    }
 }
 
 module.exports = Safe;
