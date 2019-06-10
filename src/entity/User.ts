50d3953c0150a382802ce6bbfa6f13986bec4670
FABIANO <serge.fabiano@sap.com>
2019-06-10 15:00:01 +0200

    Allow Admins to change User's Role

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -366,8 +366,10 @@ export default class User extends TenantHolder {
         filteredRequest.role = Constants.ROLE_BASIC;
       }
     } else {
-      // Update
-      filteredRequest.role = user.getRole();
+      // Do not allow to change if not Admin
+      if (!Authorizations.isAdmin(req.user)) {
+        filteredRequest.role = user.getRole();
+      }
     }
     if (req.method === 'POST' && !filteredRequest.status) {
       filteredRequest.status = Constants.USER_STATUS_BLOCKED;
