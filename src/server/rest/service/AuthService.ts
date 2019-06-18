32ab6a5770a5e6a84db17521991c2a043076c25c
Jerome Benoit <jerome.benoit@sap.com>
2019-06-18 17:42:53 +0200

    Properly check of an undefined variable.
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- src/server/rest/service/AuthService.ts
+++ src/server/rest/service/AuthService.ts
@@ -987,7 +987,7 @@ export default class AuthService {
   }
 
   static async getTenantID(subdomain) {
-    if (!subdomain) {
+    if (typeof subdomain === 'undefined') {
       return null;
     }
     // Check
