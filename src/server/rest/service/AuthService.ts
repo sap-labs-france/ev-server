c5b118f73ad6888db36755888f12deafd67f7bb7
FABIANO <serge.fabiano@sap.com>
2019-07-01 18:31:39 +0200

    Return the same HTTP error code for 'User is locked'

--- src/server/rest/service/AuthService.ts
+++ src/server/rest/service/AuthService.ts
@@ -306,8 +306,10 @@ export default class AuthService {
           } else {
             // Return data
             throw new AppError(
-              Constants.CENTRAL_SERVER, 'User is locked',
-              Constants.HTTP_OBJECT_DOES_NOT_EXIST_ERROR, 'AuthService', 'handleLogIn',
+              Constants.CENTRAL_SERVER,
+              'User is locked',
+              Constants.HTTP_USER_LOCKED_ERROR,
+              'AuthService', 'handleLogIn',
               user.getModel());
           }
         } else {
@@ -900,7 +902,9 @@ export default class AuthService {
       // Log
       throw new AppError(
         Constants.CENTRAL_SERVER,
-        'User is locked', Constants.HTTP_USER_LOCKED_ERROR, 'AuthService', 'checkUserLogin',
+        'User is locked',
+        Constants.HTTP_USER_LOCKED_ERROR,
+        'AuthService', 'checkUserLogin',
         user.getModel()
       );
     } else {
