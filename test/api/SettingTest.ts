dd1944b975a4811e7568ac05553b33c06570bfb1
DEMESSANT <jean.pierre.demessant@sap.com>
2019-07-18 15:01:59 +0200

    #579-Setting-Checks-change-Simple-Pricing-and-SAP-CC
    Take care of restoring old simple pricing

--- test/api/SettingTest.ts
+++ test/api/SettingTest.ts
@@ -20,15 +20,25 @@ describe('Setting tests', function() {
 
   before(async function() {
     // Init values
+    testData.superCentralService = new CentralServerService(null, { email: config.get('superadmin.username'), password: config.get('superadmin.password') });
     testData.centralService = new CentralServerService('utall', { email: config.get('admin.username'), password: config.get('admin.password') });
+    testData.credentials.email = config.get('admin.username');
+    // Retrieve the tenant id from the name
+    const response = await testData.superCentralService.tenantApi.readAll({ 'Search' : 'ut-all' }, { limit: Constants.UNLIMITED, skip: 0 });
+    testData.credentials.tenantId = response ? response.data.result[0].id : '';
   });
 
   after(async function() {
     // Housekeeping
+    // Reset components before leaving
+    testData.data = JSON.parse(`{"id":"${testData.credentials.tenantId}","name":"ut-all","email":"${testData.credentials.email}","subdomain":"utall","components":{"ocpi":{"active":true,"type":"gireve"},"organization":{"active":true,"type":null},"pricing":{"active":true,"type":"simple"},"refund":{"active":true,"type":"concur"},"statistics":{"active":true,"type":null},"analytics":{"active":true,"type":null}}}`);
+    const res = await testData.superCentralService.updateEntity(
+      testData.centralService.tenantApi, testData.data);
+    expect(res.status).to.equal(200);
   });
 
 
-  describe('Success cases', () => {
+  describe('Success cases (tenant ut-all)', () => {
     it('Check that retrieving refund settings filtered by identifier returns just one result', async () => {
       // Retrieve the setting id
       const read = await testData.centralService.settingApi.readAll({ 'Identifier' : 'refund' }, { limit: Constants.UNLIMITED, skip: 0 });
@@ -80,38 +90,36 @@ describe('Setting tests', function() {
       });
       expect(read.status).to.equal(200);
       expect(read.data.count).to.equal(1);
-      // Update the setting to convergent charging
+      // Store the old setting
+      const oldSetting = read.data.result[0];
+      // Activate convergent charging
+      testData.data = JSON.parse(`{"id":"${testData.credentials.tenantId}","name":"ut-all","email":"${testData.credentials.email}","subdomain":"utall","components":{"ocpi":{"active":true,"type":"gireve"},"organization":{"active":true,"type":null},"pricing":{"active":true,"type":"convergentCharging"},"refund":{"active":true,"type":"concur"},"statistics":{"active":true,"type":null},"analytics":{"active":true,"type":null}}}`);
+      let activation = await testData.superCentralService.updateEntity(testData.centralService.tenantApi, testData.data);
+      expect(activation.status).to.equal(200);
+      // Update convergent charging setting
       testData.data = JSON.parse(`{
-            "id":"${read.data.result[0].id}",
-            "identifier":"pricing",
-            "sensitiveData":["content.convergentCharging.password"],
-            "content":{
-                "type":"convergentCharging",
-                "convergentCharging":{
-                    "url":"http://test.com",
-                    "chargeableItemName":"IN",
-                    "user":"HarryPotter",
-                    "password":"Th1sI5aFakePa55*"
-                }
-            }
-        }`);
-      const update = await testData.centralService.updateEntity(testData.centralService.settingApi, testData.data);
-      expect(update.status).to.equal(200);
-      // Go back to simple pricing and check
-      testData.data = JSON.parse(`{
-        "id":"${read.data.result[0].id}",
-        "identifier":"pricing",
-        "sensitiveData":[],
-        "content":{
-          "type":"simple",
-          "simple":{
-              "price":"1",
-              "currency":"EUR"
+          "id":"${read.data.result[0].id}",
+          "identifier":"pricing",
+          "sensitiveData":["content.convergentCharging.password"],
+          "content":{
+              "type":"convergentCharging",
+              "convergentCharging":{
+                  "url":"http://test.com",
+                  "chargeableItemName":"IN",
+                  "user":"HarryPotter",
+                  "password":"Th1sI5aFakePa55*"
+              }
           }
-        }
       }`);
-      const response = await testData.centralService.updateEntity(testData.centralService.settingApi, testData.data);
-      expect(response.status).to.equal(200);
+      let update = await testData.centralService.updateEntity(testData.centralService.settingApi, testData.data);
+      expect(update.status).to.equal(200);
+      // Activate back simple pricing
+      testData.data = JSON.parse(`{"id":"${testData.credentials.tenantId}","name":"ut-all","email":"${testData.credentials.email}","subdomain":"utall","components":{"ocpi":{"active":true,"type":"gireve"},"organization":{"active":true,"type":null},"pricing":{"active":true,"type":"simple"},"refund":{"active":true,"type":"concur"},"statistics":{"active":true,"type":null},"analytics":{"active":true,"type":null}}}`);
+      activation = await testData.superCentralService.updateEntity(testData.centralService.tenantApi, testData.data);
+      expect(activation.status).to.equal(200);
+      // Restore default simple pricing setting and check
+      update = await testData.centralService.updateEntity(testData.centralService.settingApi, oldSetting);
+      expect(update.status).to.equal(200);
     });
   });
 });
