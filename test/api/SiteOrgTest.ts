9862e964d7233ad2f4eb17e2f02de05acd718736
SCHAUB <thomas.schaub@sap.com>
2019-08-05 16:07:59 +0200

    Convert Site unit tests to new framework

--- test/api/SiteOrgTest.ts
+++ test/api/SiteOrgTest.ts
@@ -1,99 +1,167 @@
 import chai, { expect } from 'chai';
 import chaiSubset from 'chai-subset';
+import CONTEXTS from './contextProvider/ContextConstants';
+import ContextProvider from './contextProvider/ContextProvider';
 import CentralServerService from '../api/client/CentralServerService';
+import TenantContext from './contextProvider/TenantContext';
+import SiteContext from './contextProvider/SiteContext';
 import Factory from '../factories/Factory';
 
 chai.use(chaiSubset);
 
 class TestData {
-  public newCompany: any;
+  public tenantContext: TenantContext;
+  public centralUserContext: any;
+  public centralUserService: CentralServerService;
+  public userContext: any;
+  public userService: CentralServerService;
+  public siteContext: SiteContext;
+  public siteAreaContext: any;
   public newSite: any;
-  public newSiteArea: any;
   public newUser: any;
+  public createdSites: any[] = [];
+  public createdUsers: any[] = [];
 }
 
 const testData: TestData = new TestData();
 
-describe('Site tests', function() {
-  this.timeout(30000);
+describe('Site tests', function () {
+  this.timeout(1000000); // Will automatically stop the unit test after that period of time
 
-  describe('Success cases', () => {
+  before(async () => {
+    chai.config.includeStack = true;
+    await ContextProvider.DefaultInstance.prepareContexts();
+  });
+
+  afterEach(() => {
+    // Can be called after each UT to clean up created data
+  });
+
+  after(async () => {
+    // Final clean up at the end
+    await ContextProvider.DefaultInstance.cleanUpCreatedContent();
+  });
+
+  describe('With component Organization (tenant ut-org)', () => {
+
     before(async () => {
-      // Create the Company
-      testData.newCompany = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.companyApi, Factory.company.build());
-      expect(testData.newCompany).to.not.be.null;
-      // Create User
-      testData.newUser = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build());
-      expect(testData.newUser).to.not.be.null;
+      testData.tenantContext = await ContextProvider.DefaultInstance.getTenantContext(CONTEXTS.TENANT_CONTEXTS.TENANT_ORGANIZATION);
+      testData.centralUserContext = testData.tenantContext.getUserContext(CONTEXTS.USER_CONTEXTS.DEFAULT_ADMIN);
+      testData.centralUserService = new CentralServerService(
+        testData.tenantContext.getTenant().subdomain,
+        testData.centralUserContext
+      );
     });
 
     after(async () => {
-      // Delete the Company
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.companyApi, testData.newCompany);
-      // Delete User
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
+      // Delete any created site
+      testData.createdSites.forEach(async (site) => {
+        await testData.centralUserService.deleteEntity(
+          testData.centralUserService.siteApi,
+          site,
+          false
+        );
+      });
+      testData.createdSites = [];
+      // Delete any created user
+      testData.createdUsers.forEach(async (user) => {
+        await testData.centralUserService.deleteEntity(
+          testData.centralUserService.userApi,
+          user,
+          false
+        );
+      });
+      testData.createdUsers = [];
     });
 
-    it('Should create a new site', async () => {
-      // Check
-      expect(testData.newCompany).to.not.be.null;
-      // Create the entity
-      testData.newSite = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.siteApi, Factory.site.build(
-          { companyID: testData.newCompany.id }));
+    describe('Where admin user', () => {
+
+      before(async () => {
+        testData.userContext = testData.tenantContext.getUserContext(CONTEXTS.USER_CONTEXTS.DEFAULT_ADMIN);
+        if (testData.userContext === testData.centralUserContext) {
+          // Reuse the central user service (to avoid double login)
+          testData.userService = testData.centralUserService;
+        } else {
+          testData.userService = new CentralServerService(
+            testData.tenantContext.getTenant().subdomain,
+            testData.userContext
+          );
+        }
+      });
+
+      it('Should be able to create a new site', async () => {
+        // Create the entity
+        testData.newSite = await testData.userService.createEntity(
+          testData.userService.siteApi,
+          Factory.site.build({ companyID: testData.tenantContext.getContext().companies[0].id })
+        );
+        testData.createdSites.push(testData.newSite);
+      });
+
+      it('Should find the created site by id', async () => {
+        // Check if the created entity can be retrieved with its id
+        await testData.userService.getEntityById(
+          testData.userService.siteApi,
+          testData.newSite
+        );
+      });
+
+      it('Should find the created site in the site list', async () => {
+        // Check if the created entity is in the list
+        await testData.userService.checkEntityInList(
+          testData.userService.siteApi,
+          testData.newSite
+        );
+      });
+
+      it('Should be able to update the site', async () => {
+        // Change entity
+        testData.newSite.name = 'New Name';
+        // Update
+        await testData.userService.updateEntity(
+          testData.userService.siteApi,
+          testData.newSite
+        );
+      });
+
+      it('Should find the updated site by id', async () => {
+        // Check if the updated entity can be retrieved with its id
+        const updatedSite = await testData.userService.getEntityById(
+          testData.userService.siteApi,
+          testData.newSite
+        );
+        // Check
+        expect(updatedSite.name).to.equal(testData.newSite.name);
+      });
+
+      it('Should be able to delete the created site', async () => {
+        // Delete the created entity
+        await testData.userService.deleteEntity(
+          testData.userService.siteApi,
+          testData.newSite
+        );
+      });
+
+      it('Should not find the deleted site with its id', async () => {
+        // Check if the deleted entity cannot be retrieved with its id
+        await testData.userService.checkDeletedEntityById(
+          testData.userService.siteApi,
+          testData.newSite
+        );
+      });
+
+      it('Should not be able to create a site without a company', async () => {
+        // Try to create the entity
+        const response = await testData.userService.createEntity(
+          testData.userService.siteApi,
+          Factory.site.build(),
+          false
+        );
+        expect(response.status).to.equal(500);
+      });
+
     });
 
-    it('Should find the created site by id', async () => {
-      // Check if the created entity can be retrieved with its id
-      await CentralServerService.DefaultInstance.getEntityById(
-        CentralServerService.DefaultInstance.siteApi, testData.newSite);
-    });
-
-    it('Should find the created site in the site list', async () => {
-      // Check if the created entity is in the list
-      await CentralServerService.DefaultInstance.checkEntityInList(
-        CentralServerService.DefaultInstance.siteApi, testData.newSite);
-    });
-
-    it('Should update the site', async () => {
-      // Change entity
-      testData.newSite.name = 'New Name';
-      // Update
-      await CentralServerService.DefaultInstance.updateEntity(
-        CentralServerService.DefaultInstance.siteApi, testData.newSite);
-    });
-
-    it('Should find the updated site by id', async () => {
-      // Check if the updated entity can be retrieved with its id
-      const updatedSite = await CentralServerService.DefaultInstance.getEntityById(
-        CentralServerService.DefaultInstance.siteApi, testData.newSite);
-      // Check
-      expect(updatedSite.name).to.equal(testData.newSite.name);
-    });
-
-    it('Should delete the created site', async () => {
-      // Delete the created entity
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.siteApi, testData.newSite);
-    });
-
-    it('Should not find the deleted site with its id', async () => {
-      // Check if the deleted entity cannot be retrieved with its id
-      await CentralServerService.DefaultInstance.checkDeletedEntityById(
-        CentralServerService.DefaultInstance.siteApi, testData.newSite);
-    });
   });
 
-  describe('Error cases', () => {
-    it('Should not create a site area without a site', async () => {
-      // Create the entity
-      const response = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.siteApi, Factory.site.build(), false);
-      expect(response.status).to.equal(500);
-    });
-  });
 });
