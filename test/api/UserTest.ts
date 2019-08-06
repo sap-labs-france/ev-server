6e7aa1970adcbf33257e0693b9d34b038c1073f7
TomTomMag <49149995+TomTomMag@users.noreply.github.com>
2019-08-06 11:20:38 +0200

    Convert User test unit to new context framework

--- test/api/UserTest.ts
+++ test/api/UserTest.ts
@@ -1,137 +1,246 @@
 import chai, { expect } from 'chai';
 import chaiSubset from 'chai-subset';
+import CONTEXTS from './contextProvider/ContextConstants';
+import ContextProvider from './contextProvider/ContextProvider';
 import CentralServerService from '../api/client/CentralServerService';
+import TenantContext from './contextProvider/TenantContext';
 import Factory from '../factories/Factory';
 
 chai.use(chaiSubset);
 
 class TestData {
+  public tenantContext: TenantContext;
+  public centralUserContext: any;
+  public centralUserService: CentralServerService;
+  public userContext: any;
+  public userService: CentralServerService;
   public newUser: any;
+  public createdUsers: any[] = [];
 }
 
 const testData: TestData = new TestData();
 
 describe('User tests', function() {
-  this.timeout(30000);
+  this.timeout(1000000); // Will automatically stop the unit test after that period of time
 
-  describe('Success cases', () => {
-    it('Should create a new user', async () => {
-      // Create
-      testData.newUser = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build());
-      // Remove Passwords
-      delete testData.newUser.passwords;
-    });
-
-    it('Should find the created user by id', async () => {
-      // Check if the created entity can be retrieved with its id
-      await CentralServerService.DefaultInstance.getEntityById(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
-    });
-
-    it('Should find the created user in the user list', async () => {
-      // Check if the created entity is in the list
-      await CentralServerService.DefaultInstance.checkEntityInList(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
-    });
-
-    it('Should update the user', async () => {
-      // Change entity
-      testData.newUser.name = 'New Name';
-      // Update
-      await CentralServerService.DefaultInstance.updateEntity(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
-    });
-
-    it('Should find the updated user by id', async () => {
-      // Check if the updated entity can be retrieved with its id
-      const updatedUser = await CentralServerService.DefaultInstance.getEntityById(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
-      // Check
-      expect(updatedUser.name).to.equal(testData.newUser.name);
-    });
-
-    it('Should delete the created user', async () => {
-      // Delete the created entity
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
-    });
-
-    it('Should not find the deleted user with its id', async () => {
-      // Check if the deleted entity cannot be retrieved with its id
-      await CentralServerService.DefaultInstance.checkDeletedEntityById(
-        CentralServerService.DefaultInstance.userApi, testData.newUser);
-    });
+  before(async () => {
+    chai.config.includeStack = true;
+    await ContextProvider.DefaultInstance.prepareContexts();
   });
 
-  describe('Find Users In Error', () => {
-    it('Should not find an active user', async () => {
-      const user = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build({ status: 'A' }));
-      const response = await CentralServerService.DefaultInstance.userApi.readAllInError({}, { limit: 100, skip: 0 });
-      expect(response.status).to.equal(200);
-      response.data.result.forEach((u) => {
-        return expect(u.id).to.not.equal(user.id);
-      });
-
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, user);
-    });
-
-    it('Should find a pending user', async () => {
-      const user = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build({ status: 'P' }));
-      const response = await CentralServerService.DefaultInstance.userApi.readAllInError({}, { limit: 100, skip: 0 });
-      expect(response.status).to.equal(200);
-      const found = response.data.result.find((u) => {
-        return u.id === user.id;
-      });
-      expect(found).to.not.be.null;
-
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, user);
-    });
-
-    it('Should find a blocked user', async () => {
-      const user = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build({ status: 'B' }));
-      const response = await CentralServerService.DefaultInstance.userApi.readAllInError({}, { limit: 100, skip: 0 });
-      expect(response.status).to.equal(200);
-      const found = response.data.result.find((u) => {
-        return u.id === user.id;
-      });
-      expect(found).to.not.be.null;
-
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, user);
-    });
-
-    it('Should find a locked user', async () => {
-      const user = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build({ status: 'L' }));
-      const response = await CentralServerService.DefaultInstance.userApi.readAllInError({}, { limit: 100, skip: 0 });
-      expect(response.status).to.equal(200);
-      const found = response.data.result.find((u) => {
-        return u.id === user.id;
-      });
-      expect(found).to.not.be.null;
-
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, user);
-    });
-
-    it('Should find an inactive user', async () => {
-      const user = await CentralServerService.DefaultInstance.createEntity(
-        CentralServerService.DefaultInstance.userApi, Factory.user.build({ status: 'I' }));
-      const response = await CentralServerService.DefaultInstance.userApi.readAllInError({}, { limit: 100, skip: 0 });
-      expect(response.status).to.equal(200);
-      const found = response.data.result.find((u) => {
-        return u.id === user.id;
-      });
-      expect(found).to.not.be.null;
-
-      await CentralServerService.DefaultInstance.deleteEntity(
-        CentralServerService.DefaultInstance.userApi, user);
-    });
+  afterEach(() => {
+    // Can be called after each UT to clean up created data
   });
+
+  after(async () => {
+    // Final clean up at the end
+    await ContextProvider.DefaultInstance.cleanUpCreatedContent();
+  });
+
+  describe('With component Organization (tenant ut-org)', () => {
+
+    before(async () => {
+      testData.tenantContext = await ContextProvider.DefaultInstance.getTenantContext(CONTEXTS.TENANT_CONTEXTS.TENANT_ORGANIZATION);
+      testData.centralUserContext = testData.tenantContext.getUserContext(CONTEXTS.USER_CONTEXTS.DEFAULT_ADMIN);
+      testData.centralUserService = new CentralServerService(
+        testData.tenantContext.getTenant().subdomain,
+        testData.centralUserContext
+      );
+    });
+
+    after(async () => {
+      // Delete any created user
+      testData.createdUsers.forEach(async (user) => {
+        await testData.centralUserService.deleteEntity(
+          testData.centralUserService.userApi,
+          user,
+          false
+        );
+      });
+      testData.createdUsers = [];
+    });
+
+    describe('Where admin user', () => {
+
+      before(async () => {
+        testData.userContext = testData.tenantContext.getUserContext(CONTEXTS.USER_CONTEXTS.DEFAULT_ADMIN);
+        if (testData.userContext === testData.centralUserContext) {
+          // Reuse the central user service (to avoid double login)
+          testData.userService = testData.centralUserService;
+        } else {
+          testData.userService = new CentralServerService(
+            testData.tenantContext.getTenant().subdomain,
+            testData.userContext
+          );
+        }
+      });
+
+      describe('Using various basic APIs', () => {
+
+        it('Should be able to create a new user', async () => {
+          // Create
+          testData.newUser = await testData.userService.createEntity(
+            testData.userService.userApi,
+            Factory.user.build()
+          );
+          // Remove Passwords
+          delete testData.newUser.passwords;
+          testData.createdUsers.push(testData.newUser);
+        });
+
+        it('Should find the created user by id', async () => {
+          // Check if the created entity can be retrieved with its id
+          await testData.userService.getEntityById(
+            testData.userService.userApi,
+            testData.newUser
+          );
+        });
+
+        it('Should find the created user in the user list', async () => {
+          // Check if the created entity is in the list
+          await testData.userService.checkEntityInList(
+            testData.userService.userApi,
+            testData.newUser
+          );
+        });
+
+        it('Should be able to update the user', async () => {
+          // Change entity
+          testData.newUser.name = 'New Name';
+          // Update
+          await testData.userService.updateEntity(
+            testData.userService.userApi,
+            testData.newUser
+          );
+        });
+
+        it('Should find the updated user by id', async () => {
+          // Check if the updated entity can be retrieved with its id
+          const updatedUser = await testData.userService.getEntityById(
+            testData.userService.userApi,
+            testData.newUser
+          );
+          // Check
+          expect(updatedUser.name).to.equal(testData.newUser.name);
+        });
+
+        it('Should be able to delete the created user', async () => {
+          // Delete the created entity
+          await testData.userService.deleteEntity(
+            testData.userService.userApi,
+            testData.newUser
+          );
+        });
+
+        it('Should not find the deleted user with its id', async () => {
+          // Check if the deleted entity cannot be retrieved with its id
+          await testData.userService.checkDeletedEntityById(
+            testData.userService.userApi,
+            testData.newUser
+          );
+        });
+
+      });
+      describe('Using function "readAllInError"', () => {
+
+        it('Should not find an active user in error', async () => {
+          const user = await testData.userService.createEntity(
+            testData.userService.userApi,
+            Factory.user.build({ status: 'A' })
+          );
+          testData.createdUsers.push(user);
+          const response = await testData.userService.userApi.readAllInError({}, { limit: 100, skip: 0 });
+          expect(response.status).to.equal(200);
+          response.data.result.forEach((u) => {
+            return expect(u.id).to.not.equal(user.id);
+          });
+
+          await testData.userService.deleteEntity(
+            testData.userService.userApi,
+            user
+          );
+        });
+
+        it('Should find a pending user', async () => {
+          const user = await testData.userService.createEntity(
+            testData.userService.userApi,
+            Factory.user.build({ status: 'P' })
+          );
+          testData.createdUsers.push(user);
+          const response = await testData.userService.userApi.readAllInError({}, { limit: 100, skip: 0 });
+          expect(response.status).to.equal(200);
+          const found = response.data.result.find((u) => {
+            return u.id === user.id;
+          });
+          expect(found).to.not.be.null;
+
+          await testData.userService.deleteEntity(
+            testData.userService.userApi,
+            user
+          );
+        });
+
+        it('Should find a blocked user', async () => {
+          const user = await testData.userService.createEntity(
+            testData.userService.userApi,
+            Factory.user.build({ status: 'B' })
+          );
+          testData.createdUsers.push(user);
+          const response = await testData.userService.userApi.readAllInError({}, { limit: 100, skip: 0 });
+          expect(response.status).to.equal(200);
+          const found = response.data.result.find((u) => {
+            return u.id === user.id;
+          });
+          expect(found).to.not.be.null;
+
+          await testData.userService.deleteEntity(
+            testData.userService.userApi,
+            user
+          );
+        });
+
+        it('Should find a locked user', async () => {
+          const user = await testData.userService.createEntity(
+            testData.userService.userApi,
+            Factory.user.build({ status: 'L' })
+          );
+          testData.createdUsers.push(user);
+          const response = await testData.userService.userApi.readAllInError({}, { limit: 100, skip: 0 });
+          expect(response.status).to.equal(200);
+          const found = response.data.result.find((u) => {
+            return u.id === user.id;
+          });
+          expect(found).to.not.be.null;
+
+          await testData.userService.deleteEntity(
+            testData.userService.userApi,
+            user
+          );
+        });
+
+        it('Should find an inactive user', async () => {
+          const user = await testData.userService.createEntity(
+            testData.userService.userApi,
+            Factory.user.build({ status: 'I' })
+          );
+          testData.createdUsers.push(user);
+          const response = await testData.userService.userApi.readAllInError({}, { limit: 100, skip: 0 });
+          expect(response.status).to.equal(200);
+          const found = response.data.result.find((u) => {
+            return u.id === user.id;
+          });
+          expect(found).to.not.be.null;
+
+          await testData.userService.deleteEntity(
+            testData.userService.userApi,
+            user
+          );
+        });
+
+      });
+
+    });
+
+  });
+
 });
