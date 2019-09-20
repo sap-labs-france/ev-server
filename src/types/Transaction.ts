ae68575a0c4bd72590b16186e0b231d00a845741
FABIANO <sergefabiano@free.fr>
2019-09-20 23:19:54 +0200

    Add missing transaction prop

--- src/types/Transaction.ts
+++ src/types/Transaction.ts
@@ -25,6 +25,7 @@ export default interface Transaction {
     extraInactivitySecs: number;
     totalConsumption: number;
     totalDurationSecs: number;
+    currentTotalDurationSecs: number;
     timestamp: Date;
     transactionData?: any;
     signedData?: any;
