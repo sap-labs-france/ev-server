095a669ee56a08607d6e7eea39be8a60b48d4366
FABIANO <sergefabiano@free.fr>
2019-10-25 15:40:40 +0200

    Removed i18n tests

--- src/utils/I18nManager.ts
+++ src/utils/I18nManager.ts
@@ -12,24 +12,10 @@ export default class I18nManager {
     require("moment/locale/en-gb");
     // Get translation files
     i18n.translations['en'] = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/i18n/en.json`, 'utf8'));
-    console.log('====================================');
-    console.log(i18n.translations['en']);
-    console.log('====================================');
-    console.log()
     i18n.translations['fr'] = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/i18n/fr.json`, 'utf8'));
     // Default
     i18n.locale = Constants.DEFAULT_LANGUAGE;
     moment.locale(Constants.DEFAULT_LANGUAGE);
-
-    // Set the locale
-    I18nManager.switchLocale('en');
-    const title = i18n.t('notifications.endOfSession.title',
-      { chargeBoxID: 'ChargerID', connectorId: 'ConnectorID' });
-    const body = i18n.t('notifications.endOfSession.body',
-      { chargeBoxID: 'ChargerID', connectorId: 'ConnectorID' });
-    console.log('====================================');
-    console.log({title, body});
-    console.log('====================================');
   }
 
   public static switchLocale(locale: string) {
