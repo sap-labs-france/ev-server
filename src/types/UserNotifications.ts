ef547b0d65c9fbfed7f11f474807765ce5d18aab
FABIANO <sergefabiano@free.fr>
2019-11-03 07:08:24 +0100

    Add notif type in push notif

--- src/types/UserNotifications.ts
+++ src/types/UserNotifications.ts
@@ -27,6 +27,19 @@ export type UserNotificationKeys =
  'sendSmtpAuthError'
 ;
 
+export enum UserNotificationType {
+  SESSION_STARTED = 'SessionStarted',
+  OPTIMAL_CHARGE_REACHED = 'OptimalChargeReached',
+  END_OF_CHARGE = 'EndOfCharge',
+  END_OF_SESSION = 'EndOfSession',
+  USER_ACCOUNT_STATUS_CHANGED = 'UserAccountStatusChanged',
+  UNKNOWN_USER_BADGED = 'UnknownUserBadged',
+  CHARGING_STATION_STATUS_ERROR = 'ChargingStationStatusError',
+  CHARGING_STATION_REGISTERED = 'ChargingStationRegistered',
+  OCPI_PATCH_STATUS_ERROR = 'OcpiPatchStatusError',
+  SMTP_AUTH_ERROR = 'SmtpAuthError'
+};
+
 export type InactivityStatusLevel =
  'info' |
  'warning' |
