1c8f981cb7de4c600899662ca7f7670566517545
sebastien.savalle@gmail.com <sebastien.savalle@gmail.com>
2019-11-13 16:03:13 +0100

    only allow userToken in AppAuthError

--- src/exception/AppAuthError.ts
+++ src/exception/AppAuthError.ts
@@ -6,7 +6,7 @@ export default class AppAuthError extends Error {
 
   constructor(readonly params: {
     action: string; entity: string; value?: string; errorCode: number; module: string;
-    method: string; user: UserToken | User; actionOnUser?: User | UserToken | string;
+    method: string; user: UserToken; actionOnUser?: User | UserToken | string;
   }) {
     super(`Role ${Utils.getRoleNameFromRoleID(params.user.role)} is not authorized to perform ${params.action} on ${params.entity}${(params.value ? ' \'' + params.value + '\'' : '')}`);
   }
