85557fec827d21a0bdf5750e6e3fadadf3bcb23e
FABIANO <sergefabiano@free.fr>
2019-12-20 17:17:25 +0100

    Update the charger specs factory

--- src/integration/charging-station-specifics/ChargingStationSpecificsFactory.ts
+++ src/integration/charging-station-specifics/ChargingStationSpecificsFactory.ts
@@ -1,9 +1,16 @@
 import ChargingStation from '../../types/ChargingStation';
 import ChargingStationSpecifics from './ChargingStationSpecifics';
+import Constants from '../../utils/Constants';
+import SchneiderChargingStationSpecifics from './schneider/SchneiderChargingStationSpecifics';
 
 export default class ChargingStationSpecificsFactory {
 
-  static async getChargingStationSpecifics(chargingStation: ChargingStation): Promise<ChargingStationSpecifics> {
+  static async getChargingStationSpecificsInstance(chargingStation: ChargingStation): Promise<ChargingStationSpecifics> {
+    switch (chargingStation.chargePointVendor) {
+      // Schneider
+      case Constants.VENDOR_SCHNEIDER:
+        return new SchneiderChargingStationSpecifics(chargingStation);
+    }
     return null;
   }
 }
