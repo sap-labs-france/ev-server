88d3e07a765bbeb97716db4fd1b9cc64013fe8d8
sebastien.savalle@gmail.com <sebastien.savalle@gmail.com>
2020-01-17 16:01:55 +0100

    Create consumption details when a session is updated

--- src/server/ocpi/ocpi-services-impl/ocpi-2.1.1/EMSPSessionsEndpoint.ts
+++ src/server/ocpi/ocpi-services-impl/ocpi-2.1.1/EMSPSessionsEndpoint.ts
@@ -8,12 +8,14 @@ import AbstractOCPIService from '../../AbstractOCPIService';
 import UserStorage from '../../../../storage/mongodb/UserStorage';
 import TransactionStorage from '../../../../storage/mongodb/TransactionStorage';
 import { OCPISession, OCPISessionStatus } from '../../../../types/ocpi/OCPISession';
-import Transaction from '../../../../types/Transaction';
+import Transaction, { InactivityStatus } from '../../../../types/Transaction';
 import moment from 'moment';
 import ChargingStationStorage from '../../../../storage/mongodb/ChargingStationStorage';
 import { OCPILocation } from '../../../../types/ocpi/OCPILocation';
 import Utils from '../../../../utils/Utils';
 import { OCPIResponse } from '../../../../types/ocpi/OCPIResponse';
+import Consumption from '../../../../types/Consumption';
+import ConsumptionStorage from '../../../../storage/mongodb/ConsumptionStorage';
 
 const EP_IDENTIFIER = 'sessions';
 const MODULE_NAME = 'EMSPSessionsEndpoint';
@@ -176,23 +178,71 @@ export default class EMSPSessionsEndpoint extends AbstractEndpoint {
         userID: user.id,
         tagID: session.auth_id,
         timestamp: session.start_datetime,
-        lastUpdate: session.last_updated,
         chargeBoxID: chargingStation.id,
         timezone: Utils.getTimezone(chargingStation.coordinates),
         connectorId: connectorId,
         meterStart: 0,
-        currentTotalConsumption: session.kwh * 1000,
         stateOfCharge: 0,
         currentStateOfCharge: 0,
         currentTotalInactivitySecs: 0,
-        ocpiSession: session
+        pricingSource: 'ocpi',
+        currentInactivityStatus: InactivityStatus.INFO,
+        currentConsumption: 0,
+        currentConsumptionWh: 0,
+        lastMeterValue: {
+          value: 0,
+          timestamp: session.start_datetime
+        },
+        signedData: '',
       } as Transaction;
     }
+
+    if (session.kwh > 0) {
+      const consumptionWh = session.kwh * 1000 - Utils.convertToFloat(transaction.lastMeterValue.value);
+      const duration = moment(session.last_updated).diff(transaction.lastMeterValue.timestamp, 'milliseconds') / 1000;
+      if (consumptionWh > 0 || duration > 0) {
+        const sampleMultiplier = duration > 0 ? 3600 / duration : 0;
+        const currentConsumption = consumptionWh > 0 ? consumptionWh * sampleMultiplier : 0;
+        const amount = session.total_cost - transaction.price;
+
+        transaction.currentConsumption = currentConsumption;
+        transaction.currentConsumptionWh = consumptionWh > 0 ? consumptionWh : 0;
+        transaction.currentTotalConsumption = transaction.currentTotalConsumption + transaction.currentConsumptionWh;
+
+        if (consumptionWh <= 0) {
+          transaction.currentTotalInactivitySecs = transaction.currentTotalInactivitySecs + duration;
+          transaction.currentInactivityStatus = Utils.getInactivityStatusLevel(
+            transaction.chargeBox, transaction.connectorId, transaction.currentTotalInactivitySecs);
+        }
+
+        const consumption: Consumption = {
+          transactionId: transaction.id,
+          connectorId: transaction.connectorId,
+          chargeBoxID: transaction.chargeBoxID,
+          userID: transaction.userID,
+          startedAt: new Date(transaction.lastMeterValue.timestamp),
+          endedAt: new Date(session.last_updated),
+          consumption: transaction.currentConsumptionWh,
+          instantPower: Math.round(transaction.currentConsumption),
+          cumulatedConsumption: transaction.currentTotalConsumption,
+          totalInactivitySecs: transaction.currentTotalInactivitySecs,
+          totalDurationSecs: transaction.stop ?
+            moment.duration(moment(transaction.stop.timestamp).diff(moment(transaction.timestamp))).asSeconds() :
+            moment.duration(moment(transaction.lastMeterValue.timestamp).diff(moment(transaction.timestamp))).asSeconds(),
+          stateOfCharge: transaction.currentStateOfCharge,
+          amount: amount,
+          currencyCode: session.currency,
+          cumulatedAmount: session.total_cost
+        } as Consumption;
+
+        await ConsumptionStorage.saveConsumption(tenant.id, consumption);
+      }
+    }
+
     transaction.ocpiSession = session;
     transaction.lastUpdate = session.last_updated;
     transaction.price = session.total_cost;
     transaction.priceUnit = session.currency;
-    transaction.pricingSource = 'ocpi';
     transaction.roundedPrice = Utils.convertToFloat(session.total_cost.toFixed(2));
 
     transaction.lastMeterValue = {
@@ -201,6 +251,7 @@ export default class EMSPSessionsEndpoint extends AbstractEndpoint {
     };
 
     if (session.end_datetime || session.status === OCPISessionStatus.COMPLETED) {
+      const stopTimestamp = session.end_datetime ? session.end_datetime : new Date();
       transaction.stop = {
         extraInactivityComputed: false,
         extraInactivitySecs: 0,
@@ -211,10 +262,11 @@ export default class EMSPSessionsEndpoint extends AbstractEndpoint {
         roundedPrice: Utils.convertToFloat(session.total_cost.toFixed(2)),
         stateOfCharge: 0,
         tagID: session.auth_id,
-        timestamp: session.end_datetime ? session.end_datetime : new Date(),
+        timestamp: stopTimestamp,
         totalConsumption: session.kwh * 1000,
-        totalDurationSecs: Math.round(moment.duration(moment(transaction.stop.timestamp).diff(moment(transaction.timestamp))).asSeconds()),
-        totalInactivitySecs: 0,
+        totalDurationSecs: Math.round(moment.duration(moment(stopTimestamp).diff(moment(transaction.timestamp))).asSeconds()),
+        totalInactivitySecs: transaction.currentTotalInactivitySecs,
+        inactivityStatus: transaction.currentInactivityStatus,
         userID: transaction.userID
       };
     }
