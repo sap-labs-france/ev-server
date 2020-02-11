f6b46ba8cecaa49eaacab546e18625a52d5ba7fb
FABIANO <sergefabiano@free.fr>
2020-02-11 10:42:17 +0100

    Fix HTTP Auth type

--- src/exception/AppAuthError.ts
+++ src/exception/AppAuthError.ts
@@ -1,11 +1,12 @@
 import User from '../types/User';
 import UserToken from '../types/UserToken';
 import Utils from '../utils/Utils';
+import { HTTPAuthError } from '../types/HTTPError';
 
 export default class AppAuthError extends Error {
 
   constructor(readonly params: {
-    action: string; entity: string; value?: string; errorCode: number; module: string;
+    action: string; entity: string; value?: string; errorCode: HTTPAuthError; module: string;
     method: string; user: UserToken; actionOnUser?: User | UserToken | string;
   }) {
     super(`Role ${Utils.getRoleNameFromRoleID(params.user.role)} is not authorized to perform ${params.action} on ${params.entity}${(params.value ? ' \'' + params.value + '\'' : '')}`);
