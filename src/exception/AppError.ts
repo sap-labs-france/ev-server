3f48dec88c6c110e91eb095f23b40e6d5bfcf41e
OliveGerste <33114317+OliveGerste@users.noreply.github.com>
2020-02-12 09:35:05 +0100

    fixing types

--- src/exception/AppError.ts
+++ src/exception/AppError.ts
@@ -1,3 +1,4 @@
+import { Action } from '../types/Authorization';
 import { HTTPError } from '../types/HTTPError';
 import User from '../types/User';
 import UserToken from '../types/UserToken';
@@ -6,7 +7,7 @@ export default class AppError extends Error {
   constructor(readonly params: {
     source: string; message: string; errorCode: HTTPError; module: string;
     method: string; user?: User | string | UserToken; actionOnUser?: User | string | UserToken;
-    action?: any; detailedMessages?: any; ocpiError?: {status_code: number; status_message: string};
+    action?: Action; detailedMessages?: any; ocpiError?: {status_code: number; status_message: string};
   }) {
     super(params.message);
   }
