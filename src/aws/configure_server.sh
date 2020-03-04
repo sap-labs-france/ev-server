c9af3408999eb46a0084fc8daee6aa2f32d7a9e3
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-04 16:27:56 +0100

    Initial AWS appspec.yml for deployment.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ src/aws/configure_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# TODO: use env variable
+cp /opt/server/dist/assets/aws/config-rest.json /opt/server/dist/assets/config.json
---
+++ src/aws/start_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /opt/server
+npm run start:prod
