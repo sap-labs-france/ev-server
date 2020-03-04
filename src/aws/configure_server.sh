638b517785f7a7754c9dbc77f36b23f93a0b9de0
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-04 18:09:28 +0100

    AWS: Fix some files name and path.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/configure_server.sh
+++ src/aws/configure_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # TODO: use env variable
-cp /opt/server/dist/assets/aws/config-rest.json /opt/server/dist/assets/config.json
+cp /opt/server/dist/assets/configs-aws/config-rest.json /opt/server/dist/assets/config.json
