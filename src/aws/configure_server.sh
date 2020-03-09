07f9aa273818d766985a3c9249c1b796cda129d8
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-09 17:52:01 +0100

    AWS: workaround npm issue.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/configure_server.sh
+++ src/aws/configure_server.sh
@@ -2,3 +2,6 @@
 
 # TODO: use env variable
 cp /opt/server/dist/assets/configs-aws/config-rest.json /opt/server/dist/assets/config.json
+# Workaround Error: Cannot find module '..'
+cd /opt/server
+npm install
