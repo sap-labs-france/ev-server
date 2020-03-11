ca3664b58a1bb344f436068be10ba0125496efc2
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-11 19:27:05 +0100

    AWS: Cleanup debug after first server CodeDeploy success.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/afterinstall_server.sh
+++ src/aws/afterinstall_server.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-. conf.sh
-echo "debug"
-echo $install_dir
-
 install_dir=/opt/server
 
 cp $install_dir/dist/assets/configs-aws/config-rest-qa.json $install_dir/dist/assets/config.json
