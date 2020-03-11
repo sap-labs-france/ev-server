6add765d49e0763d057b39437393db57b1bbe199
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-11 18:08:30 +0100

    AWS: run npm install after installation on ec2 instance.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/afterinstall_server.sh
+++ src/aws/afterinstall_server.sh
@@ -3,8 +3,8 @@
 install_dir=/opt/server
 
 cd $install_dir
-# FIXME: use CodeBuild build
-# npm install
+npm install
+# Use CodeBuild build instead
 # npm run build:prod
 cp $install_dir/dist/assets/configs-aws/config-rest-qa.json $install_dir/dist/assets/config.json
 
