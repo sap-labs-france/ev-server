a7f502d2adab365e09edb96fe31ef63302419507
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-11 18:02:40 +0100

    AWS: try to bypass the build on the EC2 instance.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/afterinstall_server.sh
+++ src/aws/afterinstall_server.sh
@@ -4,7 +4,7 @@ install_dir=/opt/server
 
 cd $install_dir
 # FIXME: use CodeBuild build
-npm install
-npm run build:prod
+# npm install
+# npm run build:prod
 cp $install_dir/dist/assets/configs-aws/config-rest-qa.json $install_dir/dist/assets/config.json
 
