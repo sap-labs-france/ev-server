b5505185ab79a9a0e8a2a76cf254db34606ae38f
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-11 19:17:41 +0100

    AWS: the startup issue.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/start_server.sh
+++ src/aws/start_server.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-node -r source-map-support/register --stack-trace-limit=1024 /opt/server/dist/start.js
+cd /opt/server
+node -r source-map-support/register --stack-trace-limit=1024 dist/start.js
