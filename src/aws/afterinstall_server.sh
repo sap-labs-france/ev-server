805e29a3267234cf4f115e66c5b3844ed8a60d6d
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-11 18:43:55 +0100

    AWS: Do not run npm install after install on ec2 instance.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/afterinstall_server.sh
+++ src/aws/afterinstall_server.sh
@@ -3,8 +3,5 @@
 install_dir=/opt/server
 
 cd $install_dir
-npm install
-# Use CodeBuild build instead
-# npm run build:prod
 cp $install_dir/dist/assets/configs-aws/config-rest-qa.json $install_dir/dist/assets/config.json
 
