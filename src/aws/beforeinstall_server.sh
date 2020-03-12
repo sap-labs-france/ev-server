131e2e2b80fba4f0299f23568105c8ce29cba2f4
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-12 12:17:18 +0100

    AWS: before installation script must have hardcoded emobility env variables.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/beforeinstall_server.sh
+++ src/aws/beforeinstall_server.sh
@@ -4,13 +4,12 @@ if [ -e /etc/environment_emobility.sh ]
 then
   . /etc/environment_emobility.sh
 else
-  echo "/etc/environment_emobility.sh file not found, exiting"
+  echo "/etc/environment_emobility.sh file not found, fallback to hardcoded values"
+  emobility_install_dir=/opt/server
   exit 1
 fi
 
 [ -n $emobility_install_dir ] && { echo "emobility installation directory env variable not found, exiting"; exit 1; }
-[ -n $emobility_landscape ] && { echo "emobility landscape env variable not found, exiting"; exit 1; }
-[ -n $emobility_service_type ] && { echo "emobility env service type variable not found, exiting"; exit 1; }
 
 if [ -d $emobility_install_dir ]; then
   rm -rf $emobility_install_dir
