8af2793cf2f12da1125e4a35f4394c7600c5471c
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-13 15:45:22 +0100

    AWS: Change install dir owner after the installation
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/afterinstall_server.sh
+++ src/aws/afterinstall_server.sh
@@ -11,6 +11,7 @@ fi
 [ -z $emobility_install_dir ] && { echo "emobility installation directory env variable not found, exiting"; exit 1; }
 [ -z $emobility_landscape ] && { echo "emobility landscape env variable not found, exiting"; exit 1; }
 [ -z $emobility_service_type ] && { echo "emobility env service type variable not found, exiting"; exit 1; }
+[ -z $emobility_user ] && { echo "emobility user env variable not found, exiting"; exit 1; }
 
 cp $emobility_install_dir/dist/assets/configs-aws/config-$emobility_service_type-$emobility_landscape.json $emobility_install_dir/dist/assets/config.json
-
+sudo chown $emobility_user.$emobility_user $emobility_install_dir
