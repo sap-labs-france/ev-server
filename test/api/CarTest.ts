cdd4cdf4fc6ffbccd695aae4325571fe8f336fd7
Ben Romdhane <mahdi.ben.romdhane@sap.com>
2020-03-16 13:48:09 +0100

    commit to switch branch

---
+++ test/api/CarTest.ts
@@ -0,0 +1,17 @@
+import chaiSubset from 'chai-subset';
+
+chai.use(chaiSubset);
+describe('Car Service', function () {
+  this.timeout(50000);
+
+  before(() => {
+  });
+
+  after(() => {
+  });
+  describe('Success cases', () => {
+    it('Should be able to get cars', async () => {
+
+    });
+  });
+});
---
+++ test/api/client/CarApi.ts
@@ -0,0 +1,14 @@
+import CrudApi from './utils/CrudApi';
+
+export default class CarApi extends CrudApi {
+  public constructor(authenticatedApi) {
+    super(authenticatedApi);
+  }
+  public readById(id) {
+    return super.readById(id, '/client/api/Car');
+  }
+
+  public readAll(params, paging = Constants.DEFAULT_PAGING, ordering = Constants.DEFAULT_ORDERING) {
+    return super.readAll(params, paging, ordering, '/client/api/Cars');
+  }
+}
