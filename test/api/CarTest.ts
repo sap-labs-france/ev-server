7c21b7e4f50bcdb972b93b348c1b101b2992cf96
Ben Romdhane <mahdi.ben.romdhane@sap.com>
2020-03-16 18:14:37 +0100

    unit test for car module

--- test/api/CarTest.ts
+++ test/api/CarTest.ts
@@ -1,17 +1,60 @@
 import chaiSubset from 'chai-subset';
+import CentralServerService from './client/CentralServerService';
+import chai, { assert, expect } from 'chai';
+import config from '../config';
+import TestData from './client/utils/TestData';
+
 
 chai.use(chaiSubset);
-describe('Car Service', function () {
-  this.timeout(50000);
+const testData: TestData = new TestData();
+describe('Car Service', function() {
+  before(async function() {
+    // Init values
+    testData.superCentralService = new CentralServerService(null, { email: config.get('superadmin.username'), password: config.get('superadmin.password') });
 
-  before(() => {
+    testData.centralService = new CentralServerService('utnothing', {
+      email: config.get('admin.username'),
+      password: config.get('admin.password')
+    });
+
   });
 
   after(() => {
   });
   describe('Success cases', () => {
-    it('Should be able to get cars', async () => {
+    describe('Where admin user', () => {
+      it('Should be able to get cars', async () => {
+        const response = await testData.centralService.carApi.readAll({});
+        expect(response.status).to.equal(200);
+      });
 
+      it('Should be able to get car by ID', async () => {
+        const response = await testData.centralService.carApi.readById('');
+        expect(response.status).to.equal(200);
+      });
+
+      it('Should not be able to get the car Object', async () => {
+        const response = await testData.centralService.carApi.getCarObject('');
+        expect(response.status).to.equal(560);
+
+      });
+
+      describe('Where Super admin user', () => {
+        it('Should be able to get car by ID', async () => {
+          const response = await testData.centralService.carApi.readById('');
+          expect(response.status).to.equal(200);
+        });
+
+        it('Should be able to get cars', async () => {
+          const response = await testData.centralService.carApi.readAll({});
+          expect(response.status).to.equal(200);
+        });
+
+        it('Should be able to get car Object', async () => {
+          const response = await testData.superCentralService.carApiSuperTenant.getCarObject('');
+          expect(response.status).to.equal(200);
+        });
+      });
     });
   });
 });
