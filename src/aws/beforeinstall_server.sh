295b14139af5ea3a30c5fad4c3e0d824402749f0
Jérôme Benoit <jerome.benoit@sap.com>
2020-03-20 19:14:57 +0100

    AWS: use systemd to start the server.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/aws/beforeinstall_server.sh
+++ src/aws/beforeinstall_server.sh
@@ -25,3 +25,7 @@ else
   echo "authbind is not installed, exiting"
   exit 1
 fi
+
+ cp $emobility_install_dir/dist/assets/configs-aws/ev-server.service /etc/systemd/system/
+ systemctl daemon-reload
+ systemctl enable ev-server.service
