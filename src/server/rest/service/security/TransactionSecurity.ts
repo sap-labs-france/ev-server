caa8ae903c4b55fe6e8c93406cd46ae82bb42cac
OliveGerste <33114317+OliveGerste@users.noreply.github.com>
2020-05-18 11:10:31 +0200

    push amp values to front end

--- src/server/rest/service/security/TransactionSecurity.ts
+++ src/server/rest/service/security/TransactionSecurity.ts
@@ -318,10 +318,12 @@ export default class TransactionSecurity {
       const newConsumption: TransactionConsumption = {
         date: consumption.endedAt,
         instantPower: consumption.instantPower,
+        instantAmps: consumption.instantAmps,
         cumulatedConsumption: consumption.cumulatedConsumption,
         stateOfCharge: consumption.stateOfCharge,
         cumulatedAmount: consumption.cumulatedAmount,
-        limitWatts: consumption.limitWatts
+        limitWatts: consumption.limitWatts,
+        limitAmps: consumption.limitAmps
       };
       if (consumption.stateOfCharge) {
         newConsumption.stateOfCharge = consumption.stateOfCharge;
