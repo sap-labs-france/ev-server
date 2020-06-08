568bfa441e25c8b695524d225ae9c6c054dbd84b
Jérôme Benoit <jerome.benoit@sap.com>
2020-06-08 18:44:45 +0200

    Fix tenant name printing in WS sendMessage().
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/server/ocpp/json/WSConnection.ts
+++ src/server/ocpp/json/WSConnection.ts
@@ -281,6 +281,7 @@ export default class WSConnection {
     // Send a message through WSConnection
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self = this;
+    const tenant = await TenantStorage.getTenant(this.tenantID);
     // Create a promise
     return await new Promise((resolve, reject) => {
       let messageToSend;
@@ -328,7 +329,7 @@ export default class WSConnection {
         this.wsConnection.send(messageToSend);
       } else {
         // Reject it
-        return rejectCallback(`Web socket closed for Message ID '${messageId}' with content '${messageToSend}' (${TenantStorage.getTenant(this.tenantID).then((tenant) => tenant.name)})`);
+        return rejectCallback(`Web socket closed for Message ID '${messageId}' with content '${messageToSend}' (${tenant.name})`);
       }
       // Request?
       if (messageType !== MessageType.CALL_MESSAGE) {
@@ -336,7 +337,7 @@ export default class WSConnection {
         resolve();
       } else {
         // Send timeout
-        setTimeout(() => rejectCallback(`Timeout for Message ID '${messageId}' with content '${messageToSend} (${TenantStorage.getTenant(this.tenantID).then((tenant) => tenant.name)}`), Constants.OCPP_SOCKET_TIMEOUT);
+        setTimeout(() => rejectCallback(`Timeout for Message ID '${messageId}' with content '${messageToSend} (${tenant.name}`), Constants.OCPP_SOCKET_TIMEOUT);
       }
     });
   }
