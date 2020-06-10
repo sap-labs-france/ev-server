8c8cd7a03d6427e335b5b0c8ece04846fae95eca
Jérôme Benoit <jerome.benoit@sap.com>
2020-06-10 15:25:45 +0200

    Add support for secure local connexion in OCPP client.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/types/configuration/ChargingStationConfiguration.ts
+++ src/types/configuration/ChargingStationConfiguration.ts
@@ -7,4 +7,5 @@ export default interface ChargingStationConfiguration {
   notifEndOfChargePercent: number;
   notifStopTransactionAndUnlockConnector: boolean;
   useServerLocalIPForRemoteCommand?: boolean;
+  secureLocalServer?: boolean;
 }
