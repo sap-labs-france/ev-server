a6fe0ed38a74f2bcebc2360eb8bb3736f09f83fb
FABIANO <sergefabiano@free.fr>
2020-07-01 00:23:01 +0200

    Updated UpdateChargingStationTemplatesTask

--- src/migration/tasks/UpdateChargingStationTemplatesTask.ts
+++ src/migration/tasks/UpdateChargingStationTemplatesTask.ts
@@ -34,7 +34,7 @@ export default class UpdateChargingStationTemplatesTask extends MigrationTask {
   }
 
   getVersion(): string {
-    return '3.04';
+    return '3.05';
   }
 
   private async applyTemplateToChargingStations(tenant: Tenant) {
