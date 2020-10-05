18aaa08f27cf0d81b9f4bb72c0b09281aa06d618
Jérôme Benoit <jerome.benoit@sap.com>
2020-10-05 23:07:23 +0200

    Storage debug: fix method name.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/storage/mongodb/ChargingStationStorage.ts
+++ src/storage/mongodb/ChargingStationStorage.ts
@@ -88,11 +88,11 @@ export default class ChargingStationStorage {
 
   public static async deleteChargingStationTemplates(): Promise<void> {
     // Debug
-    const uniqueTimerID = Logging.traceStart(MODULE_NAME, 'saveChargingStationTemplate');
+    const uniqueTimerID = Logging.traceStart(MODULE_NAME, 'deleteChargingStationTemplates');
     // Delete all records
     await global.database.getCollection<ChargingStationTemplate>(Constants.DEFAULT_TENANT, 'chargingstationtemplates').deleteMany({});
     // Debug
-    Logging.traceEnd(MODULE_NAME, 'saveChargingStationTemplate', uniqueTimerID);
+    Logging.traceEnd(MODULE_NAME, 'deleteChargingStationTemplates', uniqueTimerID);
   }
 
   public static async saveChargingStationTemplate(chargingStationTemplate: ChargingStationTemplate): Promise<void> {
