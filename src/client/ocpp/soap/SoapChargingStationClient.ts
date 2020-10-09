dd24ebfde191ed116dba1da375c059124621e00e
FABIANO <sergefabiano@free.fr>
2020-10-09 17:42:40 +0200

    Fixed merge issue

--- src/client/ocpp/soap/SoapChargingStationClient.ts
+++ src/client/ocpp/soap/SoapChargingStationClient.ts
@@ -194,7 +194,7 @@ export default class SoapChargingStationClient extends ChargingStationClient {
     // Init SOAP Headers with the action
     this.initSoapHeaders(Command.RESET);
     // Log
-    Logging.logChargingStationClientSendAction(MODULE_NAME, this.tenantID, this.chargingStation.id, ServerAction.RESET,
+    Logging.logChargingStationClientSendAction(MODULE_NAME, this.tenantID, this.chargingStation.id, ServerAction.CHARGING_STATION_RESET,
       [params, { headers: this.client.getSoapHeaders() }]);
     // Execute
     const { error, result, envelope } = await this.client.Reset({
@@ -217,7 +217,7 @@ export default class SoapChargingStationClient extends ChargingStationClient {
       return error;
     }
     // Log
-    Logging.logChargingStationClientReceiveAction(MODULE_NAME, this.tenantID, this.chargingStation.id, ServerAction.RESET, [
+    Logging.logChargingStationClientReceiveAction(MODULE_NAME, this.tenantID, this.chargingStation.id, ServerAction.CHARGING_STATION_RESET, [
       { result },
       { envelope }
     ]);
