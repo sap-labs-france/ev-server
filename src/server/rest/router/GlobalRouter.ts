74c54ba6bb89e408fe706fbdd0576d3e7a16c7f1
FABIANO <sergefabiano@free.fr>
2020-10-14 05:53:29 +0200

    Added Util endpoint

--- src/server/rest/router/GlobalRouter.ts
+++ src/server/rest/router/GlobalRouter.ts
@@ -3,6 +3,7 @@ import { authRouter } from './auth/AuthRouter';
 import express from 'express';
 import { swaggerRouter } from './doc/SwaggerRouter';
 import { tenantRouter } from './api/TenantRouter';
+import { utilRouter } from './util/UtilRouter';
 
 export const globalRouter = express.Router();
 
@@ -12,6 +13,8 @@ globalRouter.use('/v1/api', AuthService.authenticate(), tenantRouter);
 // Auth
 globalRouter.use('/v1/auth', authRouter);
 
+// Util
+globalRouter.use('/v1/util', utilRouter);
+
 // Docs
 globalRouter.use('/v1/docs', swaggerRouter);
-
