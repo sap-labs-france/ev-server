95f1bbcdabfe7df4eaca72ad92c2051ccad815ad
Jérôme Benoit <jerome.benoit@sap.com>
2020-10-15 10:31:22 +0200

    Simplify endpoints URI string.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/server/rest/router/api/TenantRouter.ts
+++ src/server/rest/router/api/TenantRouter.ts
@@ -7,11 +7,11 @@ import sanitize from 'mongo-sanitize';
 
 export const tenantRouter = express.Router();
 
-tenantRouter.get('/' + ServerAction.REST_TENANTS, async (req: Request, res: Response, next: NextFunction) => {
+tenantRouter.get(`/${ServerAction.REST_TENANTS}`, async (req: Request, res: Response, next: NextFunction) => {
   await RouterUtils.handleServerAction(TenantService.handleGetTenants.bind(this), ServerAction.REST_TENANTS, req, res, next);
 });
 
-tenantRouter.get('/' + ServerAction.REST_TENANTS + '/:id', async (req: Request, res: Response, next: NextFunction) => {
+tenantRouter.get(`/${ServerAction.REST_TENANTS}/:id`, async (req: Request, res: Response, next: NextFunction) => {
   req.query.ID = sanitize(req.params.id);
   await RouterUtils.handleServerAction(TenantService.handleGetTenant.bind(this), ServerAction.REST_TENANTS, req, res, next);
 });
