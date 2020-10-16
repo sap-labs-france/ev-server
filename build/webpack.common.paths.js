270de60c5885dae5cf661ee291c23fc72ff9a47b
Jérôme Benoit <jerome.benoit@sap.com>
2020-10-16 14:23:17 +0200

    Apply security updates.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- build/webpack.common.paths.js
+++ build/webpack.common.paths.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 module.exports = {
   outputPath: path.join(__dirname, '../dist'),
-  srcPath: path.join(__dirname, '../src')
+  srcPath: path.join(__dirname, '../src'),
+  buildPath: path.join(__dirname, '../build')
 };
