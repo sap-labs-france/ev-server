0eb2fb0baa775a7be054580e55cfa5bc9aba7d79
mahdibenromdhane <mahdi.benromdhane@esprit.tn>
2020-11-02 12:59:04 +0100

    Regex latitude support value between (-1 & 1)

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -78,7 +78,7 @@ export default class Constants {
   public static readonly MAX_DATE = new Date('9999-12-31Z23:59:59:999');
   public static readonly MIN_DATE = new Date('1970-01-01Z00:00:00:000');
 
-  public static readonly REGEX_VALIDATION_LATITUDE = /^-?([1-8]?[1-9]|[1-9]0)\.{0,1}[0-9]*$/;
+  public static readonly REGEX_VALIDATION_LATITUDE = /^-?([1-8]?[0-9]|[0-9]0)\.{0,1}[0-9]*$/;
   public static readonly REGEX_VALIDATION_LONGITUDE = /^-?([1]?[0-7][0-9]|[1]?[0-8][0]|[1-9]?[0-9])\.{0,1}[0-9]*$/;
   public static readonly MAX_GPS_DISTANCE_METERS = 40000000; // Earth
 
