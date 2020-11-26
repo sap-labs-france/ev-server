c42ddf4936e1b9fab6bf411e33b2e55ae6fb5e2c
Christopher Boehm <christopher.boehm@sap.com>
2020-11-26 13:09:40 +0100

    undo skip

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -2,7 +2,8 @@ import static com.sap.piper.internal.Prerequisites.checkScript
 
 void call(Map params) {
 
-  echo "skip acceptance stage"
+  //echo "skip acceptance stage"
+  params.originalStage()
 
 }
 return this
