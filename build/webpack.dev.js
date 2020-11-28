671b323bd9fafcecd84e1df3815030b1d7272d14
FABIANO <sergefabiano@free.fr>
2020-11-28 18:51:37 +0100

    Use nodemon

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,25 +1,10 @@
 
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-
 const config = {
   mode: 'development',
   optimization: {
     nodeEnv: 'development-build'
   },
   plugins: [
-    new CircularDependencyPlugin({
-      // exclude detection of files based on a RegExp
-      exclude: /a\.js|node_modules/,
-      // include specific files based on a RegExp
-      include: /src/,
-      // add errors to webpack instead of warnings
-      failOnError: false,
-      // allow import cycles that include an asyncronous import,
-      // e.g. via import(/* webpackMode: "weak" */ './file.js')
-      allowAsyncCycles: false,
-      // set the current working directory for displaying module paths
-      cwd: process.cwd(),
-    })
   ]
 };
 
