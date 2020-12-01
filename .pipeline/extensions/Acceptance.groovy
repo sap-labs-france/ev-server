0095036082f5830af85b31e952c70931dd5d280d
Christopher Boehm <christopher.boehm@sap.com>
2020-12-01 12:09:19 +0100

    Update Acceptance.groovy

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -2,8 +2,8 @@ import static com.sap.piper.internal.Prerequisites.checkScript
 
 void call(Map params) {
 
-  echo "skip acceptance stage"
-  //params.originalStage()
+  echo "do acceptance stage"
+  params.originalStage()
 
 }
 return this
