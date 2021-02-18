9b3332bcd92451eda1bc9e1855d30e5c754500be
LucasBrazi06 <sergefabiano@free.fr>
2021-02-18 09:32:48 +0100

    Remove Unlock connector for Basic user

--- src/authorization/AuthorizationsDefinition.ts
+++ src/authorization/AuthorizationsDefinition.ts
@@ -128,11 +128,6 @@ const AUTHORIZATION_DEFINITION: AuthorizationDefinition = {
       { resource: Entity.CHARGING_STATIONS, action: Action.LIST, attributes: ['*'] },
       {
         resource: Entity.CHARGING_STATION,
-        action: [Action.READ, Action.UNLOCK_CONNECTOR],
-        attributes: ['*']
-      },
-      {
-        resource: Entity.CHARGING_STATION,
         action: [Action.REMOTE_START_TRANSACTION, Action.AUTHORIZE, Action.START_TRANSACTION],
         attributes: ['*'],
         condition: {
