55a5a6c416ecc14bacf2a8c06f2e30aa0ccd860c
Loan ALOUACHE <alouache.loan@gmail.com>
2021-03-10 14:04:05 +0100

    Fixed param name

--- src/server/rest/v1/service/security/ChargingStationSecurity.ts
+++ src/server/rest/v1/service/security/ChargingStationSecurity.ts
@@ -281,7 +281,7 @@ export default class ChargingStationSecurity {
 
   public static filterChargingStationGetFirmwareRequest(request: any): HttpChargingStationGetFirmwareRequest {
     return {
-      fileName: sanitize(request.FileName),
+      fileName: sanitize(request.fileName),
     };
   }
 
