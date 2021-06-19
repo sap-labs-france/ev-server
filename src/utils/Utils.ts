445c9214f6c0678d1c5383d81fd64e5a76c7f922
LucasBrazi06 <sergefabiano@free.fr>
2021-06-19 17:38:57 +0200

    Perfs size reduction

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1492,6 +1492,7 @@ export default class Utils {
     module: string; method: string; action: ServerAction|string; group?: PerformanceRecordGroup;
     httpUrl?: string; httpMethod?: string; httpCode?: number; chargingStationID?: string,
   }): PerformanceRecord {
+    const cpuInfo = os.cpus();
     return {
       tenantID: params.tenantID,
       timestamp: new Date(),
@@ -1501,11 +1502,11 @@ export default class Utils {
       process: cluster.isWorker ? 'worker ' + cluster.worker.id.toString() : 'master',
       processMemoryUsage: process.memoryUsage(),
       processCPUUsage: process.cpuUsage(),
-      cpusInfo: os.cpus(),
+      numberOfCPU: cpuInfo.length,
+      modelOfCPU: cpuInfo.length > 0 ? cpuInfo[0].model : '',
       memoryTotalGb: Utils.createDecimal(os.totalmem()).div(Constants.ONE_BILLION).toNumber(),
       memoryFreeGb: Utils.createDecimal(os.freemem()).div(Constants.ONE_BILLION).toNumber(),
       loadAverageLastMin: os.loadavg()[0],
-      networkInterface: os.networkInterfaces(),
       numberOfChargingStations: global.centralSystemJsonServer?.getNumberOfJsonConnections(),
       source: params.source,
       module: params.module,
