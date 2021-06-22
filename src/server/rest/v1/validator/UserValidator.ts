3ca7e0b13b02970a2841e86ca3c92674eb76b617
Loan ALOUACHE <alouache.loan@gmail.com>
2021-06-22 14:55:04 +0200

    Fixed typo

--- src/server/rest/v1/validator/UserValidator.ts
+++ src/server/rest/v1/validator/UserValidator.ts
@@ -1,10 +1,8 @@
 import User, { ImportedUser } from '../../../../types/User';
 
-import Authorizations from '../../../../authorization/Authorizations';
 import Schema from '../../../../types/validator/Schema';
 import SchemaValidator from './SchemaValidator';
 import UserToken from '../../../../types/UserToken';
-import Utils from '../../../../utils/Utils';
 import fs from 'fs';
 import global from '../../../../types/GlobalType';
 
@@ -12,13 +10,11 @@ export default class UserValidator extends SchemaValidator {
   private static instance: UserValidator|null = null;
   private importedUserCreation: Schema;
   private userCreate: Schema;
-  private userAdminCreate: Schema;
 
   private constructor() {
     super('UserValidator');
     this.importedUserCreation = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/user/imported-user-create-req.json`, 'utf8'));
     this.userCreate = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/user/user-create.json`, 'utf8'));
-    this.userAdminCreate = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/user/user-admin-create.json`, 'utf8'));
   }
 
   public static getInstance(): UserValidator {
@@ -32,7 +28,7 @@ export default class UserValidator extends SchemaValidator {
     this.validate(this.importedUserCreation, importedUser);
   }
 
-  validateUserCreate(user: any, loggedUser: UserToken): Partial<User> {
+  validateUserCreate(user: any): Partial<User> {
     this.validate(this.userCreate, user);
     user.password = user.passwords.password;
     return user;
