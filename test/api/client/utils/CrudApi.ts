37a78430f668e1fdddaa44231f6b649eb2727b5c
Claude ROSSI <claude.rossi@sap.com>
2021-06-30 05:59:50 +0200

    final buildRestEndpointUrl

--- test/api/client/utils/CrudApi.ts
+++ test/api/client/utils/CrudApi.ts
@@ -134,21 +134,11 @@ export default class CrudApi {
   }
 
   // Build URL targeting REST endpoints
-  // protected buildRestEndpointUrl(urlPatternAsString: ServerRoute, params: { [name: string]: string | number | null } = {}): string {
-  //   let resolvedUrlPattern = urlPatternAsString as string;
-  //   for (const key in params) {
-  //     if (Object.prototype.hasOwnProperty.call(params, key)) {
-  //       resolvedUrlPattern = resolvedUrlPattern.replace(`:${key}`, encodeURIComponent(params[key]));
-  //     }
-  //   }
-  //   return resolvedUrlPattern;
-  // }
-
   protected buildRestEndpointUrl(urlPatternAsString: ServerRoute, params: { [name: string]: string | number | null } = {}): string {
     let resolvedUrlPattern = urlPatternAsString as string;
     for (const key in params) {
       if (Object.prototype.hasOwnProperty.call(params, key)) {
-        resolvedUrlPattern = resolvedUrlPattern.replace(`:${key}`, params[key] as string);
+        resolvedUrlPattern = resolvedUrlPattern.replace(`:${key}`, encodeURIComponent(params[key]));
       }
     }
     return '/v1/api/' + resolvedUrlPattern;
