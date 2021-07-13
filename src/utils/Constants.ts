fea5cda58b14b7533ff969f99375b3f2c7e31449
Claude ROSSI <claude.rossi@sap.com>
2021-07-13 17:58:08 +0200

    es_MX removed from constant definition

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -275,7 +275,7 @@ export default class Constants {
   public static readonly PWD_NUMBER_RE = /([\d])/g; // Cannot store regex in enum
   public static readonly PWD_SPECIAL_CHAR_RE = /([!#$%^&*.?-])/g; // Cannot store regex in enum
 
-  public static readonly SUPPORTED_LOCALES = Object.freeze(['en_US', 'fr_FR', 'es_MX', 'de_DE', 'pt_PT', 'it_IT']);
+  public static readonly SUPPORTED_LOCALES = Object.freeze(['en_US', 'fr_FR', 'es_ES', 'de_DE', 'pt_PT', 'it_IT']);
   public static readonly SUPPORTED_LANGUAGES = Object.freeze(['en', 'fr', 'es', 'de', 'pt', 'it']);
   public static readonly DEFAULT_LOCALE = 'en_US';
   public static readonly DEFAULT_LANGUAGE = 'en';
