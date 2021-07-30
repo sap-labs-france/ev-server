dce8b68a236324ef5a987d6714e7d68c3e7295f3
LucasBrazi06 <sergefabiano@free.fr>
2021-07-31 00:40:56 +0200

    Optimized Status Notif

--- src/server/ocpp/services/OCPPService.ts
+++ src/server/ocpp/services/OCPPService.ts
@@ -834,7 +834,7 @@ export default class OCPPService {
     if (statusNotification.status === ChargePointStatus.AVAILABLE) {
       // Get the last transaction
       const lastTransaction = await TransactionStorage.getLastTransactionFromChargingStation(
-        tenant, chargingStation.id, connector.connectorId, { withChargingStation: true, withUser: true, withTag: true });
+        tenant, chargingStation.id, connector.connectorId);
       // Transaction completed
       if (lastTransaction?.stop) {
         // Check Inactivity
@@ -863,7 +863,7 @@ export default class OCPPService {
                 lastTransaction.stop.extraInactivitySecs = 0;
               } else {
                 // Fix the Inactivity severity
-                lastTransaction.stop.inactivityStatus = Utils.getInactivityStatusLevel(lastTransaction.chargeBox, lastTransaction.connectorId,
+                lastTransaction.stop.inactivityStatus = Utils.getInactivityStatusLevel(chargingStation, lastTransaction.connectorId,
                   lastTransaction.stop.totalInactivitySecs + lastTransaction.stop.extraInactivitySecs);
                 // Build extra inactivity consumption
                 await OCPPUtils.buildExtraConsumptionInactivity(tenant, lastTransaction);
