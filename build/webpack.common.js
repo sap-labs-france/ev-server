ac2012886bd98fd337eac5e52e877cc1bfbf4aef
LucasBrazi06 <sergefabiano@free.fr>
2021-08-09 12:45:29 +0200

    Put back the config in assets

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -40,7 +40,7 @@ const config = {
     new webpack.ProgressPlugin(),
     new CopyPlugin({
       patterns: [
-        { from: 'src/assets/', to: 'assets/', globOptions: { ignore: ['**/configs*/**', '**/config-template*', '**/ups-template*', '**/cla'] } },
+        { from: 'src/assets/', to: 'assets/' },
         { from: 'src/integration/pricing/convergent-charging/assets/', to: 'integration/pricing/convergent-charging/assets/', noErrorOnMissing: true }
       ]
     })
