e779f4275fc66b49ea5b60a53cfbabba3642649a
Loan ALOUACHE <alouache.loan@gmail.com>
2021-08-18 14:37:27 +0200

    Added connection schema validation + removed security class

--- src/server/rest/v1/validator/ConnectionValidator.ts
+++ src/server/rest/v1/validator/ConnectionValidator.ts
@@ -1,3 +1,6 @@
+import { HttpConnectionRequest, HttpConnectionsRequest } from '../../../../types/requests/HttpConnectionRequest';
+
+import Connection from '../../../../types/Connection';
 import Schema from '../../../../types/validator/Schema';
 import SchemaValidator from './SchemaValidator';
 import fs from 'fs';
@@ -6,10 +9,14 @@ import global from '../../../../types/GlobalType';
 export default class ConnectionValidator extends SchemaValidator {
   private static instance: ConnectionValidator|null = null;
   private connectionCreation: Schema;
+  private connectionsGet: Schema;
+  private connectionGet: Schema;
 
   private constructor() {
     super('ConnectionValidator');
     this.connectionCreation = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/connections/connection-create.json`, 'utf8'));
+    this.connectionsGet = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/connections/connections-get.json`, 'utf8'));
+    this.connectionGet = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/connections/connection-get.json`, 'utf8'));
   }
 
   public static getInstance(): ConnectionValidator {
@@ -19,8 +26,19 @@ export default class ConnectionValidator extends SchemaValidator {
     return ConnectionValidator.instance;
   }
 
-  validateConnectionCreation(content): void {
+  validateConnectionCreation(content): Connection {
     this.validate(this.connectionCreation, content);
+    return content;
+  }
+
+  validateConnectionsGet(content): HttpConnectionsRequest {
+    this.validate(this.connectionsGet, content);
+    return content;
+  }
+
+  validateConnectionGet(content): HttpConnectionRequest {
+    this.validate(this.connectionGet, content);
+    return content;
   }
 }
 
