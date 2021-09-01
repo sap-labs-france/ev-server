4a5ea08c19237808531d236371f45536fa67a6e6
Loan ALOUACHE <alouache.loan@gmail.com>
2021-09-01 16:25:14 +0200

    Added Asset getMany / getOne / create rest endpoints

--- src/server/rest/v1/router/api/AssetRouter.ts
+++ src/server/rest/v1/router/api/AssetRouter.ts
@@ -14,12 +14,34 @@ export default class AssetRouter {
 
   public buildRoutes(): express.Router {
     this.buildRouteCreateAssetConsumption();
+    this.buildRouteCreateAsset();
+    this.buildRouteGetAssets();
+    this.buildRouteGetAsset();
     return this.router;
   }
 
   protected buildRouteCreateAssetConsumption(): void {
     this.router.post(`/${ServerRoute.REST_ASSET_CONSUMPTION}`, async (req: Request, res: Response, next: NextFunction) => {
+      req.query.assetID = req.params.id;
       await RouterUtils.handleServerAction(AssetService.handleCreateAssetConsumption.bind(this), ServerAction.ASSET_CONSUMPTION, req, res, next);
+    });
+  }
+
+  protected buildRouteCreateAsset(): void {
+    this.router.post(`/${ServerRoute.REST_ASSETS}`, async (req: Request, res: Response, next: NextFunction) => {
+      await RouterUtils.handleServerAction(AssetService.handleCreateAsset.bind(this), ServerAction.ASSET_CREATE, req, res, next);
+    });
+  }
+
+  protected buildRouteGetAssets(): void {
+    this.router.get(`/${ServerRoute.REST_ASSETS}`, async (req: Request, res: Response, next: NextFunction) => {
+      await RouterUtils.handleServerAction(AssetService.handleGetAssets.bind(this), ServerAction.ASSETS, req, res, next);
+    });
+  }
+
+  protected buildRouteGetAsset(): void {
+    this.router.get(`/${ServerRoute.REST_ASSET}`, async (req: Request, res: Response, next: NextFunction) => {
+      await RouterUtils.handleServerAction(AssetService.handleGetAsset.bind(this), ServerAction.ASSET, req, res, next);
     });
   }
 }
