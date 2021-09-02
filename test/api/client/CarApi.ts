0d72931dd09fe71b4d07ed9bd79d35c4867b32d2
Loan ALOUACHE <alouache.loan@gmail.com>
2021-09-02 17:11:47 +0200

    Added cars getOne/update/delete endpoints

--- test/api/client/CarApi.ts
+++ test/api/client/CarApi.ts
@@ -1,4 +1,5 @@
 import AuthenticatedBaseApi from './utils/AuthenticatedBaseApi';
+import { Car } from '../../../src/types/Car';
 import CrudApi from './utils/CrudApi';
 import { ServerRoute } from '../../../src/types/Server';
 import TestConstants from './utils/TestConstants';
@@ -25,22 +26,22 @@ export default class CarApi extends CrudApi {
   }
 
   public async readCar(id: string) {
-    return super.read({ ID: id }, '/client/api/Car');
+    return super.read({ ID: id }, this.buildRestEndpointUrl(ServerRoute.REST_CAR, { id }));
   }
 
   public async readCars(params, paging = TestConstants.DEFAULT_PAGING, ordering = TestConstants.DEFAULT_ORDERING) {
     return super.readAll(params, paging, ordering, this.buildRestEndpointUrl(ServerRoute.REST_CARS));
   }
 
-  public async update(data) {
-    return super.update(data, '/client/api/CarUpdate');
+  public async update(data: Car) {
+    return super.update(data, this.buildRestEndpointUrl(ServerRoute.REST_CAR, { id: data.id }));
   }
 
-  public async create(data) {
-    return super.create(data, '/client/api/CarCreate');
+  public async create(data: Car) {
+    return super.create(data, this.buildRestEndpointUrl(ServerRoute.REST_CARS));
   }
 
-  public async delete(id) {
-    return super.delete(id, '/client/api/CarDelete');
+  public async delete(id: string) {
+    return super.delete(id, this.buildRestEndpointUrl(ServerRoute.REST_CAR, { id }));
   }
 }
