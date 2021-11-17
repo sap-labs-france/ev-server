a8d542cf467f447681ab0db3d983cd3511fc923d
LucasBrazi06 <sergefabiano@free.fr>
2021-11-17 10:50:01 +0100

    Technical User cannot login to the App

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,10 +1,12 @@
-import Tenant from '../../src/types/Tenant';
-import UserToken from '../../src/types/UserToken';
+import { Details } from 'express-useragent';
+import Tenant from '../src/types/Tenant';
+import UserToken from '../src/types/UserToken';
 
 declare module 'express' {
   export interface Request {
     locale: string;
     user?: UserToken;
     tenant?: Tenant;
+    useragent?: Details;
   }
 }
