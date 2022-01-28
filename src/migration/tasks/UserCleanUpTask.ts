28a9cefaafe01a2df37d70295dbe8927daac3899
LucasBrazi06 <sergefabiano@free.fr>
2022-01-29 00:09:22 +0100

    Clean up user's data

---
+++ src/migration/tasks/UserCleanUpTask.ts
@@ -0,0 +1,24 @@
+import Tenant from '../../types/Tenant';
+import TenantMigrationTask from '../TenantMigrationTask';
+import global from '../../types/GlobalType';
+
+export default class UserCleanUpTask extends TenantMigrationTask {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  public async migrateTenant(tenant: Tenant) {
+    // Clean up
+    await global.database.getCollection(tenant.id, 'users').updateMany(
+      {}, { $unset: { lastSelectedCarID: '', lastSelectedCar: '' } });
+  }
+
+  public getVersion(): string {
+    return '1.0';
+  }
+
+  public getName(): string {
+    return 'UserCleanUpTask';
+  }
+
+  public isAsynchronous(): boolean {
+    return true;
+  }
+}
