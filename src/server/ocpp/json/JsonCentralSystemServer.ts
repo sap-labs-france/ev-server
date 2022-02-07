9642dfde239a8399ae215eaee1df864b4dba594a
LucasBrazi06 <sergefabiano@free.fr>
2022-02-07 23:09:15 +0100

    Disable debug log

--- src/server/ocpp/json/JsonCentralSystemServer.ts
+++ src/server/ocpp/json/JsonCentralSystemServer.ts
@@ -207,7 +207,7 @@ export default class JsonCentralSystemServer extends CentralSystemServer {
   private async onOpen(ws: uWS.WebSocket) {
     let wsConnection: WSConnection;
     // Debug
-    Logging.logConsoleDebug(`WS Connection - '${ws.url as string}'`);
+    // Logging.logConsoleDebug(`WS Connection - '${ws.url as string}'`);
     // Create WS Wrapper
     const wsWrapper = new WSWrapper(ws);
     // Keep it on the ws
