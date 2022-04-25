23d79f757e3e66413de9171a8aef895643e1f8a4
LucasBrazi06 <sergefabiano@free.fr>
2022-04-25 11:12:24 +0200

    Fixed typos

--- src/scheduler/tasks/CheckPreparingSessionNotStartedTask.ts
+++ src/scheduler/tasks/CheckPreparingSessionNotStartedTask.ts
@@ -35,7 +35,7 @@ export default class CheckPreparingSessionNotStartedTask extends TenantScheduler
               // Send notification
               moment.locale(siteOwners.result[0].user.locale);
               for (const connector of chargingStation.connectors) {
-                void NotificationHandler.sendPreparingSessionNotStarted(tenant, chargingStation, siteOwners.result[0].user, {
+                void NotificationHandler.sendPreparingTransactionNotStarted(tenant, chargingStation, siteOwners.result[0].user, {
                   user: siteOwners.result[0].user,
                   chargeBoxID: chargingStation.id,
                   siteID: chargingStation.siteID,
