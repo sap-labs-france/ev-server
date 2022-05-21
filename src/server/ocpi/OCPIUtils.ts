13c7940aa62cc942da33935e7b369ff4a53957d8
LucasBrazi06 <sergefabiano@free.fr>
2022-05-21 15:54:32 +0200

    Use createdOn in OCPI token when lastChangedOn is not provided

--- src/server/ocpi/OCPIUtils.ts
+++ src/server/ocpi/OCPIUtils.ts
@@ -196,7 +196,7 @@ export default class OCPIUtils {
       issuer: tenant.name,
       valid: tag.active && tag.user?.status === UserStatus.ACTIVE,
       whitelist: OCPITokenWhitelist.ALLOWED_OFFLINE,
-      last_updated: tag.lastChangedOn ?? new Date()
+      last_updated: tag.lastChangedOn ?? tag.createdOn
     };
   }
 
