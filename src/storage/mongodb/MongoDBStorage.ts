0884042bd393ba4a65e107eb36cb67b94f458f5c
LucasBrazi06 <sergefabiano@free.fr>
2022-06-13 11:29:56 +0200

    Fixed temp build errors

--- src/storage/mongodb/MongoDBStorage.ts
+++ src/storage/mongodb/MongoDBStorage.ts
@@ -77,11 +77,12 @@ export default class MongoDBStorage {
     });
     // Trigger callbacks
     changeStream.on('change', (changeStreamDocument: ChangeStreamDocument) => {
-      const documentID = changeStreamDocument.documentKey ? changeStreamDocument.documentKey['_id'] : null;
-      const documentChange = changeStreamDocument.operationType;
-      const fullDocument = changeStreamDocument.fullDocument;
-      // Callback
-      callback(documentID, documentChange as DatabaseDocumentChange, fullDocument);
+      console.log(changeStreamDocument);
+      // const documentID = changeStreamDocument.documentKey ? changeStreamDocument.documentKey['_id'] : null;
+      // const documentChange = changeStreamDocument.operationType;
+      // const fullDocument = changeStreamDocument.fullDocument;
+      // // Callback
+      // callback(documentID, documentChange as DatabaseDocumentChange, fullDocument);
     });
   }
 
