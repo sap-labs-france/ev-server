2d9397dc3c010a718c1f8bc0923ddacab5e71e7c
LucasBrazi06 <sergefabiano@free.fr>
2022-06-24 15:43:14 +0200

    Fixed wrong Json schema filename and $id

--- src/server/rest/v1/validator/CommonValidatorRest.ts
+++ src/server/rest/v1/validator/CommonValidatorRest.ts
@@ -6,7 +6,7 @@ import global from '../../../../types/GlobalType';
 
 export default class CommonValidatorRest extends SchemaValidator {
   private static instance: CommonValidatorRest | null = null;
-  private verifyTenantIdSubdomain: Schema = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/common/verify-tenant-id-subdomain.json.json`, 'utf8'));
+  private verifyTenantIdSubdomain: Schema = JSON.parse(fs.readFileSync(`${global.appRoot}/assets/server/rest/v1/schemas/common/verify-tenant-id-subdomain.json`, 'utf8'));
 
   private constructor() {
     super('CommonValidatorRest');
