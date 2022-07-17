b8516639752cdc53d8edc379094c21ab30aa0d89
LucasBrazi06 <sergefabiano@free.fr>
2022-07-17 14:03:47 +0200

    Updated all Jest packages to latest versions Use jest-circus as test runner Force Exit all unit tests

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -186,7 +186,7 @@ const config: Config.InitialOptions = {
   // testResultsProcessor: undefined,
 
   // This option allows use of a custom test runner
-  // testRunner: "jest-circus/runner",
+  testRunner: 'jest-circus/runner',
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   // testURL: "http://localhost",
