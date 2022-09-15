87620dce37ab8779ef9d56fc329e3e2afd9d1e93
BANCHEREAU Caroline <caroline.banchereau@sap.com>
2022-09-15 20:20:25 +0200

    added hashes

--- test/factories/ChargingStationTemplateFactory.ts
+++ test/factories/ChargingStationTemplateFactory.ts
@@ -4,11 +4,6 @@ import { Factory } from 'rosie';
 const chargingStationTemplate = Factory.define('chargingStationTemplate')
   .attr('template', {
     chargePointVendor: 'Schneider Electric',
-    hash: '2c4510465d76887ba5e1d04a14707e8135ef74d797c6de8dca0e7f2eebd2d28c',
-    hashTechnical: 'd112b385b7a531b1ca7b764b3f19e9c30cfb71a0296c51971a07f569dd5596b4',
-    hashCapabilities: 'b6eb6b8ec90ae246cdfa4b57f1b2b9d744ff5ce86658dea7db4b3a84137340ac',
-    hashOcppStandard: '4956a1e4b71797f2086a156b5254c53c3d761cc6d89cd151289c0405b1444fad',
-    hashOcppVendor: 'deda8999e837b15e29eeee76167eb778d053aadbf5f6d05430b490e1ca3791b1',
     technical: {
       maximumPower: 44160,
       type: 110,
@@ -63,7 +58,12 @@ const chargingStationTemplate = Factory.define('chargingStationTemplate')
         clockaligneddatainterval: true
       }
     }]
-  });
+  })
+  .attr('hash', '2c4510465d76887ba5e1d04a14707e8135ef74d797c6de8dca0e7f2eebd2d28c')
+  .attr('hashTechnical', 'd112b385b7a531b1ca7b764b3f19e9c30cfb71a0296c51971a07f569dd5596b4')
+  .attr('hashCapabilities', 'b6eb6b8ec90ae246cdfa4b57f1b2b9d744ff5ce86658dea7db4b3a84137340ac')
+  .attr('hashOcppStandard', '4956a1e4b71797f2086a156b5254c53c3d761cc6d89cd151289c0405b1444fad')
+  .attr('hashOcppVendor', 'deda8999e837b15e29eeee76167eb778d053aadbf5f6d05430b490e1ca3791b1')
 export default class ChargingStationTemplateFactory {
   static build(attributes?, options?): ChargingStationTemplate {
     return chargingStationTemplate.build(attributes, options);
