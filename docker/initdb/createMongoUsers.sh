8c0b504a91a138ff1e4f6161896b1e3d5ef360a4
Ramzi Siala <1226644+Ramzay@users.noreply.github.com>
2022-12-02 17:49:02 +0100

    testing exec on mongodb container

--- docker/initdb/createMongoUsers.sh
+++ docker/initdb/createMongoUsers.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-echo "Test"
+ADMIN_DB="admin"
+ADMIN_USERNAME="evse-admin"
+ADMIN_PASSWORD="evse-admin-pwd"
+MONGODB_PORT=27017
+
+echo "db.getSiblingDB(\"$ADMIN_DB\").createUser({user: \"$ADMIN_USERNAME\",pwd: \"$ADMIN_PASSWORD\",roles: [ { role: \"readWrite\", db: \"$ADMIN_DB\" } ]});"
+
+docker exec --tty mongodb mongo --port $MONGODB_PORT --eval "
+db.getSiblingDB(\"$ADMIN_DB\")
+    .createUser({
+        user: \"$ADMIN_USERNAME\",
+        pwd: \"$ADMIN_PASSWORD\",
+        roles: [ { 
+            role: \"readWrite\", 
+            db: \"$ADMIN_DB\" 
+            } 
+        ]
+    }
+);"
+ 
+echo "Result $?"
 exit 1
