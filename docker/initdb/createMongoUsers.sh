202aa2ab7c73acf3ed90d8de761b1f0b4738d154
Ramzi Siala <1226644+Ramzay@users.noreply.github.com>
2022-12-02 18:09:30 +0100

    Insert super admin in collection

--- docker/initdb/createMongoUsers.sh
+++ docker/initdb/createMongoUsers.sh
@@ -13,15 +13,16 @@ db.getSiblingDB(\"$DB\")
     .createUser({
         user: \"$USER\",
         pwd: \"$PASSWORD\",
-        roles: [ { 
-            role: \"readWrite\", 
-            db: \"$DB\" 
-            } 
+        roles: [
+            { 
+                role: \"readWrite\", 
+                db: \"$DB\" 
+            }
         ]
     }
 );"
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Error at creating user"
     exit 1
 fi
@@ -44,7 +45,72 @@ db.getSiblingDB(\"$DB\")
     }
 );"
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Error at creating user"
+    exit 1
+fi
+
+docker exec --tty mongodb mongo --port $MONGODB_PORT --eval "
+db.getCollection('default.users').insert({
+  _id: ObjectId(),
+  email: 'super.admin@ev.com',
+  address: {
+    address1: null,
+    address2: null,
+    postalCode: null,
+    city: null,
+    department: null,
+    region: null,
+    country: null,
+    coordinates: [
+      0,
+      0
+    ]
+  },
+  costCenter: null,
+  createdBy: null,
+  createdOn: ISODate('2020-04-02T00:00:00.000+0000'),
+  deleted: false,
+  firstName: 'Super',
+  iNumber: null,
+  issuer: true,
+  lastChangedBy: null,
+  locale: 'en_US',
+  mobile: null,
+  name: 'ADMIN',
+  notifications: {
+    sendSessionStarted: true,
+    sendOptimalChargeReached: true,
+    sendEndOfCharge: true,
+    sendEndOfSession: true,
+    sendUserAccountStatusChanged: true,
+    sendSessionNotStarted: true,
+    sendCarSynchronizationFailed: true,
+    sendUserAccountInactivity: true,
+    sendPreparingSessionNotStarted: false,
+    sendBillingSynchronizationFailed: false,
+    sendNewRegisteredUser: false,
+    sendUnknownUserBadged: false,
+    sendChargingStationStatusError: false,
+    sendChargingStationRegistered: false,
+    sendOcpiPatchStatusError: false,
+    sendOicpPatchStatusError: false,
+    sendOfflineChargingStations: false
+  },
+  phone: null,
+  password: '\$2a\$10$/c.TRisu3xPAGkgTL69b7uC4SGXqDIzFJuZgHOB1D.fvXf5h3WWwW',
+  passwordBlockedUntil: null,
+  passwordResetHash: null,
+  passwordWrongNbrTrials: NumberInt(0),
+  eulaAcceptedHash: 'c308ac57857ce483ef1bb50fe8c1bc2bc3b5fcf067114c8b4a3a7abf9896c45f',
+  eulaAcceptedOn: ISODate('2020-04-02T00:00:00.000+0000'),
+  eulaAcceptedVersion: 28,
+  role: 'S',
+  status: 'A',
+  notificationsActive: true
+});"
+
+if [ $? -ne 0 ]; then
+    echo "Error inserting super admin"
     exit 1
 fi
