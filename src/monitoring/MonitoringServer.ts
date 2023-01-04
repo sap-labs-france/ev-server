e13d1a88985e939aed02de56582fca9955f284fd
Luc MARGARON <luc.margaron@sap.com>
2023-01-04 11:16:01 +0100

    monitoring changes

--- src/monitoring/MonitoringServer.ts
+++ src/monitoring/MonitoringServer.ts
@@ -1,4 +1,23 @@
 
+import client from 'prom-client';
+import { ComposedMonitoringMetric } from './ComposedMonitoringMetric';
+
 export default abstract class MonitoringServer {
   public abstract start(): void;
+
+  public abstract getGauge(name: string): client.Gauge | undefined;
+
+  public abstract createGaugeMetric(
+    metricname: string,
+    metrichelp: string,
+    labelNames?: string[]
+  ): client.Gauge;
+
+  public abstract getComposedMetric(
+    prefix: string,
+    metricname: string,
+    suffix: number,
+    metrichelp: string,
+    labelNames: string[]
+  ): ComposedMonitoringMetric;
 }
