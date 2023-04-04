f44d841955d2509371e8c26e349eb5b1278d41ba
BANCHEREAU Caroline <caroline.banchereau@sap.com>
2023-04-04 14:35:36 +0200

    check connector availability

--- src/server/rest/v1/service/BillingService.ts
+++ src/server/rest/v1/service/BillingService.ts
@@ -347,6 +347,18 @@ export default class BillingService {
     const filteredRequest = BillingValidatorRest.getInstance().validateBillingScanPayPaymentReq(req.body);
     // Dynamic auth
     await AuthorizationService.checkAndGetPaymentIntentAuthorizations(req.tenant, req.user, filteredRequest, Action.RETRIEVE);
+    // Get charging station to check connector availablity
+    const chargingStation = await UtilsService.checkAndGetChargingStationAuthorization(
+      req.tenant, req.user, filteredRequest.chargingStationID, Action.READ, action, null, {}, true);
+    const connector = chargingStation.connectors.find(connector => connector.connectorId === filteredRequest.connectorID);
+    if (!connector.canRemoteStartTransaction) {
+      // catch connector is not available
+      throw new AppError({
+        errorCode: HTTPError.CANNOT_REMOTE_START_CONNECTOR,
+        module: MODULE_NAME, method: 'handleScanPayPaymentIntentRetrieve',
+        message: 'Connector is not available'
+      });
+    }
     let tag: Tag;
     // Check if the user exist
     const foundUser = await UserStorage.getUserByEmail(req.tenant, filteredRequest.email);
@@ -884,7 +896,7 @@ export default class BillingService {
       limit: filteredRequest.Limit,
       onlyRecordCount: filteredRequest.OnlyRecordCount
     },
-    authorizations.projectFields
+      authorizations.projectFields
     );
     if (filteredRequest.WithAuth) {
       await AuthorizationService.addAccountsAuthorizations(req.tenant, req.user, billingAccounts, authorizations);
@@ -927,7 +939,7 @@ export default class BillingService {
       limit: filteredRequest.Limit,
       onlyRecordCount: filteredRequest.OnlyRecordCount
     },
-    authorizations.projectFields
+      authorizations.projectFields
     );
     if (filteredRequest.WithAuth) {
       await AuthorizationService.addTransfersAuthorizations(req.tenant, req.user, transfers, authorizations);
@@ -1093,6 +1105,8 @@ export default class BillingService {
       passwordResetHash: null,
       passwordBlockedUntil: null
     });
+    // Assign user to all Sites with auto-assign flag
+    await UtilsService.assignCreatedUserToSites(tenant, newUser as User);
     // Get the i18n translation class
     const i18nManager = I18nManager.getInstanceForLocale(locale);
     // Create tag for the user
